de38003648a6cd2b4986f526b92da8816131a8fa
Duan Shiqiang <duanshiqiang1@gmail.com>
2019-04-29 12:06:09 +0800

    Integrate ros task devops:provision:server with terraform

--- ros/lib/ros/devops.rb
+++ ros/lib/ros/devops.rb
@@ -10,19 +10,43 @@ module Ros
 
     def provision_instance
       puts "Provision platform config '#{config.name}' of type #{config.type} in #{Ros.env} environment"
-      puts "Work dir: #{Ros.tf_root}/#{config.provider}/deployments/#{config.type}"
-      puts 'TODO: Write necessary tfvars'
-      puts 'TODO: Invoke terraform apply'
-      puts "TODO: After terraform apply, write instance IP to devops/ansible/inventory/#{config.type}"
-
-      Dir.chdir("#{Ros.tf_root}/#{config.provider}/deployments/#{config.type}") do
+      puts "Work dir: #{Ros.tf_root}/#{config.provider}/provision/#{config.type}"
+      
+      # Write necessary tfvars
+      Dir.chdir("#{Ros.tf_root}/#{config.provider}/provision/#{config.type}") do
         File.open('terraform.tfvars', 'w') do |f|
-          f.puts("ami_distro = \"#{config.ami_distro}\"")
           # write vars for domain name, hostnames, etc.
+          tf_vars = {
+            ec2_ami_distro: config.ami_distro,
+            aws_region: config.aws_region,
+            route53_zone_main_name: config.dns.domain,
+            route53_zone_this_name: config.dns.subdomain,
+            ec2_tags: config.ec2_tags,
+            ec2_instance_type: config.instance_type,
+            ec2_key_pair: config.ec2_key_pair,
+            lambda_filename: config.lambda_filename
+          }
+
+          f.puts(tf_vars.to_json())
         end
-        # system('terraform apply')
+
+        File.open('state.tf.json', 'w') do |f|
+          tf_state = {
+            terraform: {
+              backend: {
+                "#{config.tf_state.type}": config.tf_state.to_h.select {|k,v| k.to_s != 'type'} || {}
+              }
+            }
+          }
+          
+          f.puts(tf_state.to_json())
+        end
+        system('terraform init')
+        # Invoke terraform apply
+        system('terraform apply')
+
+        puts "TODO: After terraform apply, write instance IP to devops/ansible/inventory/#{config.type}"
       end
-      # puts config
     end
 
     def deploy_instance
@@ -38,11 +62,11 @@ module Ros
 
     def provision_kubernetes
       puts "Provision platform config '#{config.name}' of type #{config.type} in #{Ros.env} environment"
-      puts "Work dir: #{Ros.tf_root}/#{config.provider}/deployments/#{config.type}"
+      puts "Work dir: #{Ros.tf_root}/#{config.provider}/provision/#{config.type}"
       puts 'TODO: Write necessary tfvars'
       puts 'TODO: Invoke terraform apply'
 
-      Dir.chdir("#{Ros.tf_root}/#{config.provider}/deployments/#{config.type}") do
+      Dir.chdir("#{Ros.tf_root}/#{config.provider}/provision/#{config.type}") do
         File.open('terraform.tfvars', 'w') do |f|
           # f.puts("ami_distro = \"#{config.ami_distro}\"")
           # write vars for whatever EKS needs
