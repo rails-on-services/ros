18f3b4ae8639e1782be5f5826864461a16758e22
Robert Roach <rjayroach@gmail.com>
2019-05-25 08:00:49 +0800

    cleanup generators

--- ros/files/project/config/environments/development.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-Ros.platform.configure do |config|
-  config.compose_files += %w(compose/base.yml compose/services.yml compose/nginx.yml compose/mounts.yml)
-  config.compose_files += %w(ros/compose/services.yml ros/compose/nginx.yml ros/compose/mounts.yml) if Ros.has_ros?
-end
