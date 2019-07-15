def27722c6af9ec7f090d08092b5283bbecf1d8c
Alexey <alexeyn.lightit@gmail.com>
2019-07-15 17:12:25 +0300

    simple iam login

--- services/iam/app/controllers/users/sessions_controller.rb
+++ services/iam/app/controllers/users/sessions_controller.rb
@@ -1,35 +1,15 @@
 # frozen_string_literal: true
 
-class Users::SessionsController < Devise::SessionsController
-  respond_to :json
-  # before_action :configure_sign_in_params, only: [:create]
+class Users::SessionsController < Devise::ApplicationController
 
-  # GET /resource/sign_in
-  # def new
-  #   binding.pry
-  #   render json: {a: 'b'}
-  #   # super
-  # end
+  protected
 
-  # POST /resource/sign_in
-  # def create
-  #   user = User.find_by(email: params[:email])
-  #   # binding.pry
-  #   # render json: user
-  #   super
-  # end
+  def login_user!
+    @current_user = User.find_by(username: sign_in_params[:username])
+    @current_user&.valid_password? sign_in_params[:password]
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   # binding.pry
-  #   # devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
-  #   ActiveSupport::HashWithIndifferentAccess[email: 'test', password: 'test']
-  # end
+  def sign_in_params
+    params.require(:data).require(:attributes).permit(%i[username password tenant_id])
+  end
 end
