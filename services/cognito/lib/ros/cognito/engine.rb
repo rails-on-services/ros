8d495d8083ee1ed4c1b06b270aa2765137e9a23e
Robert Roach <rjayroach@gmail.com>
2019-07-19 04:05:22 +0000

    update service initializers

--- services/cognito/lib/ros/cognito/engine.rb
+++ services/cognito/lib/ros/cognito/engine.rb
@@ -11,26 +11,33 @@ module Ros
         g.fixture_replacement :factory_bot, dir: 'spec/factories'
       end
 
-      # Adds this gem's db/migrations path to the enclosing application's migraations_path array
-      # https://github.com/rails/rails/issues/22261
-      initializer :append_migrations do |app|
-        config.paths['db/migrate'].expanded.each do |expanded_path|
-        app.config.paths['db/migrate'] << expanded_path
-        ActiveRecord::Migrator.migrations_paths << expanded_path
-        end unless app.config.paths['db/migrate'].first.include? 'spec/dummy'
+      initializer 'service.set_platform_config', before: 'ros_core.load_platform_config' do |_app|
+        settings_path = root.join('config/settings.yml')
+        Settings.prepend_source!(settings_path) if File.exists? settings_path
+        name = self.class.parent.name.demodulize.underscore
+        Settings.prepend_source!({ service: { name: name, policy_name: name.capitalize } })
       end
 
-      initializer :console_methods do |app|
-        Ros.config.factory_paths += Dir[Pathname.new(__FILE__).join('../../../../spec/factories')]
-        Ros.config.model_paths += config.paths['app/models'].expanded
-      end if Rails.env.development?
+      initializer 'service.initialize_infra_services', after: 'ros_core.initialize_infra_services' do |app|
+      end
 
-      initializer :service_values do |app|
-        name = 'cognito' # self.class.parent.name.demodulize.underscore
-        Settings.prepend_source!({ service: { name: name, policy_name: name.capitalize } })
-        Settings.reload!
-        # Settings.service.name = name
-        # Settings.service.policy_name = name.capitalize
+      # Adds this gem's db/migrations path to the enclosing application's migraations_path array
+      # if the gem has been included in an application, i.e. it is not running in the dummy app
+      # https://github.com/rails/rails/issues/22261
+      initializer 'service.configure_migrations' do |app|
+        unless app.config.paths['db/migrate'].first.include? 'spec/dummy'
+          config.paths['db/migrate'].expanded.each do |expanded_path|
+            app.config.paths['db/migrate'] << expanded_path
+            ActiveRecord::Migrator.migrations_paths << expanded_path
+          end
+        end
+      end
+
+      initializer 'service.configure_console_methods', before: 'ros_core.configure_console_methods' do |_app|
+        if Rails.env.development?
+          Ros.config.factory_paths += Dir[Pathname.new(__FILE__).join('../../../../spec/factories')]
+          Ros.config.model_paths += config.paths['app/models'].expanded
+        end
       end
     end
   end
--- services/comm/lib/ros/comm/engine.rb
+++ services/comm/lib/ros/comm/engine.rb
@@ -12,30 +12,30 @@ module Ros
         g.fixture_replacement :factory_bot, dir: 'spec/factories'
       end
 
-      # Adds this gem's db/migrate path to the enclosing application's migrations_path array
-      # https://github.com/rails/rails/issues/22261
-      initializer :append_migrations do |app|
-        config.paths['db/migrate'].expanded.each do |expanded_path|
-        app.config.paths['db/migrate'] << expanded_path
-        ActiveRecord::Migrator.migrations_paths << expanded_path
-        end unless app.config.paths['db/migrate'].first.include? 'spec/dummy'
+      initializer 'service.set_platform_config', before: 'ros_core.load_platform_config' do |_app|
+        settings_path = root.join('config/settings.yml')
+        Settings.prepend_source!(settings_path) if File.exists? settings_path
+        name = self.class.parent.name.demodulize.underscore
+        Settings.prepend_source!({ service: { name: name, policy_name: name.capitalize } })
       end
 
-      initializer :platform_settings do |app|
-        settings_path = root.join('config/settings')
-        Settings.prepend_source!("#{settings_path}.yml")
-        Settings.reload!
-      end if File.exists? root.join('config/settings.yml')
+      # Adds this gem's db/migrations path to the enclosing application's migraations_path array
+      # if the gem has been included in an application, i.e. it is not running in the dummy app
+      # https://github.com/rails/rails/issues/22261
+      initializer 'service.configure_migrations' do |app|
+        unless Rails.root.to_s.end_with?('spec/dummy')
+          config.paths['db/migrate'].expanded.each do |expanded_path|
+            app.config.paths['db/migrate'] << expanded_path
+            ActiveRecord::Migrator.migrations_paths << expanded_path
+          end
+        end
+      end
 
-      initializer :console_methods do |app|
-        Ros.config.factory_paths += Dir[Pathname.new(__FILE__).join('../../../../spec/factories')]
-        Ros.config.model_paths += config.paths['app/models'].expanded
-      end if Rails.env.development?
-
-      initializer :service_values do |app|
-        name = self.class.parent.name.demodulize.underscore
-        Settings.service.name = name
-        Settings.service.policy_name = name.capitalize
+      initializer 'service.configure_console_methods', before: 'ros_core.configure_console_methods' do |_app|
+        if Rails.env.development?
+          Ros.config.factory_paths += Dir[Pathname.new(__FILE__).join('../../../../spec/factories')]
+          Ros.config.model_paths += config.paths['app/models'].expanded
+        end
       end
     end
   end
