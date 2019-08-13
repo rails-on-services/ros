6f1eee9707fbf2130a6190a967d899eaf5f2d871
Robert Roach <rjayroach@gmail.com>
2019-08-13 22:02:20 +0800

    push data to kafka via avro in after_commit job

--- lib/core/app/jobs/ros/stream_cloud_event_job.rb
+++ lib/core/app/jobs/ros/stream_cloud_event_job.rb
@@ -8,17 +8,12 @@ module Ros
     queue_as "#{Settings.service.name}_stream_cloud_event"
 
     def perform(object)
-      # TODO: add any gem dependencies to core_root/ros-core.gemspec
-      # TODO: Configure the fluentd yaml in whistler_root/deployment.yml
-      # TODO: update fluentd logger code in lib/ros/core/engine.rb to configure itself from deployment.yml 
-      # NOTE: when you change any code in the engine file you need to restart rails
-      # from the shell type 'src' which stop rails; rails console
-      # if no need ot restart then from shell just type 'rc'
-      # TODO: Play with it in the console
-      # TODO: Implement code to convert object to cloud event hash format
-      # TODO: serialize the data payload with avro_turf
-      # binding.pry
-      # data = { event: object.persisted?, data: object }.to_json
+      type = "#{Settings.service.name}.#{object.class.name.downcase}"
+      message_id = object.id # SecureRandom.uuid
+      # data = object.to_json
+      data = { id: object.id, username: object.username, time_zone: object.time_zone }.to_json
+      binding.pry
+      Rails.configuration.x.event_logger.log_event(type, message_id, data)
     end
   end
 end
