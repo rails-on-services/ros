369df76b761c15a548bc3aa07c1b6c6a0717d7e9
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-08-15 17:05:01 +0800

    WIP - cleanup validator

--- lib/core/app/validators/json_schema_validator.rb
+++ lib/core/app/validators/json_schema_validator.rb
@@ -5,24 +5,23 @@ require 'json_schemer'
 class JsonSchemaValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     schema_file = options.fetch(:schema)
-    validator = init_validator(schema_file, record)
+    validator = init_validator(schema_file)
 
-    return unless validator
+    if validator
+      errors = validator.validate(value).to_a
+      return true if errors.empty?
 
-    errors = validator.validate(value).to_a
-    return true if errors.empty?
-
-    record.errors.add(attribute,
-                      :json_schema_mismatch,
-                      schema: schema_file, errors: errors)
+      record.errors.add(attribute,
+                        :json_schema_mismatch,
+                        schema: schema_file, errors: errors)
+    else
+      record.errors.add(:validator, 'missing schema in validator')
+    end
   end
 
   private
 
-  def init_validator(schema_file, record)
+  def init_validator(schema_file)
     return JSONSchemer.schema(schema_file) if schema_file && File.exist?(schema_file)
-
-    record.errors.add(:validator, 'missing schema in validator')
-    nil
   end
 end
