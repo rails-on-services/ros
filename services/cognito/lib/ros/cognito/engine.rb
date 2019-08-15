0a6b99203a334ae3812c236e308e14898ccb13a5
Alexey <alexeyn.lightit@gmail.com>
2019-08-15 16:30:52 +0300

    cognito metabaes token

--- services/cognito/lib/ros/cognito/engine.rb
+++ services/cognito/lib/ros/cognito/engine.rb
@@ -14,8 +14,8 @@ module Ros
       # https://github.com/rails/rails/issues/22261
       initializer :append_migrations do |app|
         config.paths['db/migrate'].expanded.each do |expanded_path|
-        app.config.paths['db/migrate'] << expanded_path
-        ActiveRecord::Migrator.migrations_paths << expanded_path
+          app.config.paths['db/migrate'] << expanded_path
+          ActiveRecord::Migrator.migrations_paths << expanded_path
         end unless app.config.paths['db/migrate'].first.include? 'spec/dummy'
       end
 
@@ -27,6 +27,10 @@ module Ros
       initializer :service_values do |app|
         name = self.class.parent.name.demodulize.underscore
         Settings.prepend_source!({ service: { name: name, policy_name: name.capitalize } })
+      end
+
+      initializer :autoload_services, after: :load_config_initializers do |app|
+        Dir[Rails.root.join('../../services/**/*.rb')].each { |file| require file }
       end
     end
   end
---
+++ services/cognito/services/metabase/token_generator.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'jwt'
+module Metabase
+  class TokenGenerator
+    include ActiveModel::Model
+    delegate :minimum_expiry, :maximum_expiry, to: :config
+    attr_reader :exp, :payload, :config
+
+    validate :config_validation!
+    validates :payload, presence: true
+    validates :exp, numericality: {
+      less_than_or_equal_to: :maximum_expiry,
+      greater_than_or_equal_to: :minimum_expiry
+    }, allow_blank: true
+
+    def initialize(payload:, expiry:)
+      @config = Metabase::Config.new(Rails.configuration.try(:metabase))
+
+      @exp = expiry || config.default_expiry
+      @payload = payload.merge(iat: Time.now.to_i, exp: Time.now.to_i + exp)
+    end
+
+    def token
+      return unless valid?
+
+      @token ||= JWT.encode payload, config.secret, config.sign_algorithm
+    end
+
+    private
+
+    def config_validation!
+      errors.add(:config, config.errors.messages) unless config.valid?
+    end
+  end
+end
