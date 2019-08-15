1e89ba395d639d9fa82c4962dc2eabe7ac2168f8
Alexey <alexeyn.lightit@gmail.com>
2019-08-15 16:48:13 +0300

    monkeypatch for JSONAPI::LinkBuilder

---
+++ lib/core/lib/ros/url_builder.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Ros
+  module UrlBuilder
+    class JSONAPI::LinkBuilder
+
+      def base_url
+        return @base_url unless service_engine_name
+
+        "#{@base_url}/#{service_engine_name}"
+      end
+
+      private
+
+      def service_engine_name
+        primary_resource_klass.superclass.to_s.split("::")[0...-1].first&.downcase
+      end
+
+    end
+  end
+end
