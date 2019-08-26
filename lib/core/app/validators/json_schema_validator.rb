8d94f2240e829e41d1f559f5b4ca859005b77213
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2019-08-27 01:01:40 +0800

    Break the code to small functions

--- lib/core/app/validators/json_schema_validator.rb
+++ lib/core/app/validators/json_schema_validator.rb
@@ -7,34 +7,51 @@ class JsonSchemaValidator < ActiveModel::EachValidator
     schema_file = options.fetch(:schema)
     validator = init_validator(schema_file)
 
-    if validator
-      result = validator.validate(value).to_a
-      return true if result.empty?
-
-      details = result[0]["details"]
-      fallback_error = "'#{result[0]["data_pointer"].gsub(/^\//, '')}' property is invalid"
-
-      record.errors.add(
-        attribute,
-        :json_schema_mismatch,
-        schema: schema_file,
-        errors: details.nil? ?
-          fallback_error :
-          details,
-        errors_summary: details.nil? ?
-          fallback_error :
-          details
-            .map { |k, v| "#{k}: #{v.kind_of?(Array) ? v.join(', ') : v}" }
-            .join('; ')
-      )
-    else
-      record.errors.add(:validator, 'missing schema in validator')
+    unless validator
+      return false
     end
+
+    result = validator.validate(value).to_a
+    return true if result.empty?
+
+    record.errors.add(
+      attribute,
+      :json_schema_mismatch,
+      schema: schema_file,
+      errors: errors(result),
+      errors_summary: errors_summary(result)
+    )
   end
 
   private
 
   def init_validator(schema_file)
-    return JSONSchemer.schema(schema_file) if schema_file && File.exist?(schema_file)
+    unless schema_file && File.exist?(schema_file)
+      record.errors.add(:validator, 'missing schema in validator')
+      return nil
+    end
+
+    JSONSchemer.schema(schema_file)
   end
+
+  def fallback_error(result)
+    "'#{result[0]["data_pointer"].gsub(/^\//, '')}' property is invalid"
+  end
+
+  def errors(result)
+    details = result[0]["details"]
+
+    details.nil? ? fallback_error(result) : details
+  end
+
+  def errors_summary(result)
+    details = result[0]["details"]
+
+    return details.nil? ?
+      fallback_error(result) :
+      details
+        .map { |k, v| "#{k}: #{v.kind_of?(Array) ? v.join(', ') : v}" }
+        .join('; ')
+  end
+
 end
