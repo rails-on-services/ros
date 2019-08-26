42d58ca8778f93fe22a8f13da0025c33e3838569
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-08-26 13:54:58 +0800

    [Hotfix] Fixed specs

--- services/iam/spec/rails_helper.rb
+++ services/iam/spec/rails_helper.rb
@@ -1,7 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-File.expand_path('../dummy/config/environment.rb', __FILE__)
+require File.expand_path('../dummy/config/environment.rb', __FILE__)
 # Prevent database truncation if the environment is production
 require 'rspec/rails'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
