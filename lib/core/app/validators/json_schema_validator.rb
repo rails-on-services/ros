a9b3daafceb4f94606f1843c5b3bb9a7c9027cbc
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2019-08-27 11:11:52 +0800

    [PW-682] Improve JSON schema error message
    Will show those handy messages for invalid `display_properties` definition:
    - `display_properties - does not match specified schema: 'button' property is invalid` - if property  does not match requirements, or not specified in schema
    - `display_properties - does not match specified schema: missing_keys: title` - if required property is missing

--- lib/core/app/validators/json_schema_validator.rb
+++ lib/core/app/validators/json_schema_validator.rb
@@ -7,21 +7,44 @@ class JsonSchemaValidator < ActiveModel::EachValidator
     schema_file = options.fetch(:schema)
     validator = init_validator(schema_file)
 
-    if validator
-      result = validator.validate(value).to_a
-      return true if result.empty?
+    return false unless validator
 
-      record.errors.add(attribute,
-                        :json_schema_mismatch,
-                        schema: schema_file, errors: result[0]["details"])
-    else
-      record.errors.add(:validator, 'missing schema in validator')
-    end
+    result = validator.validate(value).to_a
+    return true if result.empty?
+
+    record.errors.add(attribute, :json_schema_mismatch,
+                      schema: schema_file, errors: errors(result),
+                      errors_summary: errors_summary(result))
   end
 
   private
 
   def init_validator(schema_file)
-    return JSONSchemer.schema(schema_file) if schema_file && File.exist?(schema_file)
+    unless schema_file && File.exist?(schema_file)
+      record.errors.add(:validator, 'missing schema in validator')
+      return nil
+    end
+
+    JSONSchemer.schema(schema_file)
+  end
+
+  def fallback_error(result)
+    "'#{result[0]['data_pointer'].gsub(%r{^\/}, '')}' property is invalid"
+  end
+
+  def errors(result)
+    details = result[0]['details']
+
+    details.nil? ? fallback_error(result) : details
+  end
+
+  def errors_summary(result)
+    details = result[0]['details']
+
+    if details.nil?
+      fallback_error(result)
+    else
+      details.map { |k, v| "#{k}: #{v.is_a?(Array) ? v.join(', ') : v}" }.join('; ')
+    end
   end
 end
