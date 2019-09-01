3fd73d95600cdfe21d3fbbe3aab09eef43e715ac
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-09-01 09:44:15 +0800

    [Hotfix] Added record count to returned results

--- lib/core/lib/ros/core/engine.rb
+++ lib/core/lib/ros/core/engine.rb
@@ -111,6 +111,8 @@ module Ros
           config.default_paginator = :paged
           config.default_page_size = 10
           config.maximum_page_size = 20
+          config.top_level_meta_include_record_count = true
+          config.top_level_meta_record_count_key = :record_count
           config.top_level_meta_include_page_count = true
           config.top_level_meta_page_count_key = :page_count
         end
