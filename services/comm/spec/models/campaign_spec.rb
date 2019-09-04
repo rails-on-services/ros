391ddab86aae1889b87ad9ed0c2a045d7cf5474a
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-09-04 16:15:19 +0800

    [Hotfix] removed first attempt on shared matcher

--- services/comm/spec/models/campaign_spec.rb
+++ services/comm/spec/models/campaign_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'ros/matchers/belongs_to_resource_matcher'
 
 RSpec.describe Campaign, type: :model do
   it_behaves_like 'it belongs_to_resource', :owner
