2b359b2e02b79343aebf884177c0f4465f6682e0
Robert Roach <rjayroach@gmail.com>
2019-09-04 20:57:12 +0800

    prefer tr over gsub

--- lib/core/lib/ros/dtrace_middleware.rb
+++ lib/core/lib/ros/dtrace_middleware.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: true
 
 module Ros
-	class DtraceMiddleware
-    DTRACE_HEADERS = %w(HTTP_X_REQUEST_ID HTTP_X_B3_TRACEID HTTP_X_B3_SPANID HTTP_X_B3_PARENTSPANID
-      HTTP_X_B3_SAMPLED HTTP_X_B3_FLAGS HTTP_X_OT_SPAN_CONTEXT).freeze
+  class DtraceMiddleware
+    DTRACE_HEADERS = %w[HTTP_X_REQUEST_ID HTTP_X_B3_TRACEID HTTP_X_B3_SPANID HTTP_X_B3_PARENTSPANID
+                        HTTP_X_B3_SAMPLED HTTP_X_B3_FLAGS HTTP_X_OT_SPAN_CONTEXT].freeze
 
-		def initialize(app)
-			@app = app
-		end
+    def initialize(app)
+      @app = app
+    end
 
-		def call(env)
+    def call(env)
       Ros::Sdk::Credential.request_headers = env.slice(*DTRACE_HEADERS)
-      Ros::Sdk::Credential.request_headers.transform_keys! { |key| key[5..-1].downcase.gsub('_', '-') }
-			status, headers, response = @app.call(env)
-			[status, headers, response]
-		end
-	end
+      Ros::Sdk::Credential.request_headers.transform_keys! { |key| key[5..-1].downcase.tr('_', '-') }
+      status, headers, response = @app.call(env)
+      [status, headers, response]
+    end
+  end
 end
