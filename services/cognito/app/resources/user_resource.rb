df3434975fde4e260eab3579ea24cde8fdcd038e
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2019-09-12 00:41:02 +0800

    Add 'query' filtering to Cognito::User

--- services/cognito/app/resources/user_resource.rb
+++ services/cognito/app/resources/user_resource.rb
@@ -4,5 +4,13 @@ class UserResource < Cognito::ApplicationResource
   attributes :title, :first_name, :last_name, :phone_number, :email_address, :primary_identifier, :properties
   filter :primary_identifier
 
+  filter :query, apply: ->(records, value, _options) {
+    filter_fields = %w(primary_identifier first_name last_name)
+      .map { |field| "#{field} like '%#{CGI.escape(value[0])}%'" }
+      .join(' OR ')
+
+    records.where(filter_fields)
+  }
+
   has_many :pools
 end
