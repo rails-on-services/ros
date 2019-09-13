e4eb96dd44a532978cceb103b357403647dc7862
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2019-09-13 22:57:12 +0800

    Add email adress

--- services/cognito/app/resources/user_resource.rb
+++ services/cognito/app/resources/user_resource.rb
@@ -5,7 +5,7 @@ class UserResource < Cognito::ApplicationResource
   filter :primary_identifier
 
   filter :query, apply: ->(records, value, _options) {
-    filter_fields = %w(primary_identifier first_name last_name)
+    filter_fields = %w(primary_identifier first_name last_name email_address)
       .map { |field| "#{field} like '%#{CGI.escape(value[0])}%'" }
       .join(' OR ')
 
