8ab92a962fca701040162d6f1c53ede84bb750b1
Robert Roach <rjayroach@gmail.com>
2019-09-17 01:42:24 +0800

    Get linter to pass on iam service

--- services/iam/app/models/role_policy_join.rb
+++ services/iam/app/models/role_policy_join.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RolePolicyJoin < Iam::ApplicationRecord
   belongs_to :role
   belongs_to :policy
--- services/iam/lib/ros/iam.rb
+++ services/iam/lib/ros/iam.rb
@@ -11,7 +11,7 @@ require 'ros/iam/engine'
 
 module Ros
   class << self
-    def excluded_models; %w(Tenant Root) end
+    def excluded_models; %w[Tenant Root] end
   end
   module Iam
     # Your code goes here...
