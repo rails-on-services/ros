6d830893040635f171e4e999b14a618cb527729f
Sergey Toy <sergeytoy@perxtech.com>
2019-09-16 09:00:34 +0800

    Transfer map specs and model implemented

--- services/storage/app/models/column_map.rb
+++ services/storage/app/models/column_map.rb
@@ -2,4 +2,12 @@
 
 class ColumnMap < Storage::ApplicationRecord
   belongs_to :transfer_map
+  validate :eligible_column_name
+
+  private
+
+  def eligible_column_name
+    service_columns = transfer_map.service_name.constantize.where(model_name: transfer_map.target).first['model_columns']
+    errors.add(:illegible_column_name, "#{name} is illegible column_name") unless service_columns.include?(name)
+  end
 end
--- services/storage/spec/models/column_map_spec.rb
+++ services/storage/spec/models/column_map_spec.rb
@@ -3,5 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe ColumnMap, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:file_fingerprint) { double(where: [{ 'model_columns' => ['doesntcare'] }]) }
+  let(:transfer_map) { FactoryBot.create(:transfer_map) }
+
+  it 'can be created' do
+    stub_const 'Ros::Whatever::FileFingerprint', file_fingerprint
+    expect(1).to eq(2)
+  end
 end
