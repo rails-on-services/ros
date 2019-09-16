dc51cd445fda8609f88487bafc1bbd7a7a5282cb
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2019-09-16 19:47:38 +0800

    Use case insensitive match

--- services/cognito/app/resources/user_resource.rb
+++ services/cognito/app/resources/user_resource.rb
@@ -6,7 +6,7 @@ class UserResource < Cognito::ApplicationResource
 
   filter :query, apply: ->(records, value, _options) {
     filter_fields = %w(primary_identifier first_name last_name email_address)
-      .map { |field| "#{field} like '%#{CGI.escape(value[0])}%'" }
+      .map { |field| "#{field} ILIKE '%#{CGI.escape(value[0])}%'" }
       .join(' OR ')
 
     records.where(filter_fields)
