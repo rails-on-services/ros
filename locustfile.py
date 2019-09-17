32ae31593826f9e1fe01a359b0e1b8fd65df5280
Shi Hao <shihaotan91@gmail.com>
2019-09-17 18:35:27 +0800

    remove unnecssary code

--- locustfile.py
+++ locustfile.py
@@ -10,9 +10,6 @@ FILE = 'tmp/runtime/development/be/application/mounted/platform/credentials.json
 # def config():
 #   return json.loads(open(FILE).read())
 
-# def config(self):
-#   return json.loads(open(FILE).read())
-
 # def access_key_id(self):
 #   return self.config()[2]['credential']['access_key_id']
 
@@ -20,7 +17,6 @@ FILE = 'tmp/runtime/development/be/application/mounted/platform/credentials.json
 #   return self.config()[2]['secret']  
 
 def authorization():
-  # pdb.set_trace()
   return "Basic AGKQQTPKMZQYQJIMIAMM:Li3hJQOXrwtxp_ytIXM8jv2w7d_OX62CBQ4O9d-SOTSKUO3Egw6MfA"
 
 # def create_iam_user(self):
@@ -35,10 +31,6 @@ def login_cognito_user(self, identifier):
   payload = { "data": { "attributes": { "primary_identifier": identifier } } }
   return self.client.post('/cognito/login', data=json.dumps(payload), headers={"authorization": authorization(), 'content-type': 'application/vnd.api+json' } )
 
-# def create_user(self):
-#   payload =  { "data": { "type": "users", "attributes": { "username": "nicolas", "time_zone": "SGT" } } }
-#   return self.client.post('iam/users', data=json.dumps(payload), headers={"authorization": authorization() } )
-
 class SurveyEngagement(TaskSet):
   def setup(self):
     identifier = Faker().name()
@@ -48,12 +40,13 @@ class SurveyEngagement(TaskSet):
     self.token = login_response.headers['Authorization']
     self.content_type = 'application/vnd.api+json'
 
-  def on_start(self):  
-    batch_response = self.create_voucher_batch()
-    self.batch_id = json.loads(batch_response.content)['data'][0]['id']
-
+  def on_start(self):
+    pdb.set_trace()
     engagement_response = self.create_survey_engagement()
     self.engagement_id = json.loads(engagement_response.content)['data']['id']
+
+    batch_response = self.create_voucher_batch()
+    self.batch_id = json.loads(batch_response.content)['data'][0]['id']
 
     campaign_response = self.create_campaign_entity()
     self.campaign_id = json.loads(campaign_response.content)['data']['id']
@@ -100,7 +93,7 @@ class SurveyEngagement(TaskSet):
     return self.client.post('/organization/orgs', data=json.dumps(payload), headers={ "authorization": self.token, 'content-type': self.content_type } )
 
   def create_reward_entity(self):
-    payload = { "data": { "type": "entities", "attributes": { "name": "Starbucks Voucher", "reward_type": "Text", "category": "Voucher", "redemption_type": "Text", "cost_of_reward": 200, "organization_id": self.org_id } } }   
+    payload = { "data": { "type": "entities", "attributes": { "name": "Starbucks Voucher", "reward_type": "Perx::Reward::Entity", "category": "Perx::Reward::Entity", "redemption_type": "Text", "cost_of_reward": 200, "organization_id": self.org_id } } }   
     return self.client.post('/reward/entities', data=json.dumps(payload), headers={ "authorization": self.token, 'content-type': self.content_type } )
 
   @task(5)
@@ -117,37 +110,6 @@ class SurveyEngagement(TaskSet):
   def create_survey_answers(self):
     payload = { "data": { "type": "answers", "attributes": { "engagement_id": self.engagement_id, "campaign_entity_id": self.campaign_id, "content": { "something": "good" } } } }
     self.client.post('/survey/answers', data=json.dumps(payload), headers={ "authorization": self.token, 'content-type': self.content_type } )  
-  
-  # def authorization(self):
-    # return "Basic %s:%s" % (self.access_key_id(), self.secret_access_key())
-    # return "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vaWFtLmxvY2FsaG9zdDozMDAwIiwic3ViIjoidXJuOndoaXN0bGVyOmlhbTo6MjIyMjIyMjIyOnVzZXIvQWRtaW5fMiIsInNjb3BlIjoiKiIsImF1ZCI6WyJodHRwOi8vbG9jYWxob3N0OjMwMDAiXSwiaWF0IjoxNTY3NTc1MTI1fQ.I7y4FLu1gk5lNTwesp52SvKD4FuzxJcYtCMKj4V6nLM"
-    # return "Basic ADHWNYMMJMNLNEFNFMJU:90SUtEj9hxiKzl6O_aX_YH6lGIRJwVkXo2y_TTp9w4LlbV_lFXhzlQ"
-    # return "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2lhbS5hcGkud2hpc3RsZXIucGVyeHRlY2gub3JnIiwic3ViIjoidXJuOnBlcng6aWFtOjoyMjIyMjIyMjI6dXNlci9BZG1pbl8yIiwic2NvcGUiOiIqIiwiYXVkIjpbImh0dHBzOi8vYXBpLndoaXN0bGVyLnBlcnh0ZWNoLm9yZyJdLCJpYXQiOjE1Njc4MzY5MjB9.fyCT7D74FMNhFpuUJAn9Nxpxu3pMLBJ5bsHMw_L_bQE"
-
-  # @task(5)
-  # def post_iam_users(self):
-  #   payload =  { "data": { "type": "users", "attributes": { "username": Faker().name(), "time_zone": "SGT" } } }
-  #   self.client.post('/iam/users', data=json.dumps(payload), headers={"authorization": self.token, 'content-type': 'application/vnd.api+json'})
-
-  # @task(2)
-  # def get_iam_users(self):
-  #   self.client.get('/iam/users', headers={"authorization": self.authorization()})
-
-  # @task(2)
-  # def get_cognito_users(self):
-  #   self.client.get('/cognito/users', headers={"authorization": self.authorization()})
-
-  # @task(2)
-  # def cognito_login(self):
-  #   payload = { "data": { "attributes": { "primary_identifier": "miller" } } }
-  #   self.client.post('/cognito/login', data=json.dumps(payload), headers={ "authorization": self.authorization(), 'content-type': 'application/vnd.api+json' } )
-
-  #  @task
-  #  def shake(self):
-  #      with self.client.put("/v4/games/2", catch_response=True) as response:
-  #          if response.status_code != 200:
-  #              response.failure(response.content)
-
 
 class SurveyEngagementService(HttpLocust):
   task_set = SurveyEngagement
