54b4c5a9be44d358000f551931900f6c60a58a9e
Shi Hao <shihaotan91@gmail.com>
2019-09-17 12:58:13 +0800

    login in and posting a possible outcome is working

--- locustfile.py
+++ locustfile.py
@@ -6,32 +6,38 @@ import json
 FILE = 'tmp/runtime/development/be/application/mounted/platform/credentials.json'
 
 class IamTests(TaskSet):
-    def config(self):
-      return json.loads(open(FILE).read())
+  def config(self):
+    return json.loads(open(FILE).read())
 
-    def access_key_id(self):
-      return self.config()[2]['credential']['access_key_id']
+  def access_key_id(self):
+    return self.config()[2]['credential']['access_key_id']
 
-    def secret_access_key(self):
-      return self.config()[2]['secret']
+  def secret_access_key(self):
+    return self.config()[2]['secret']
 
-    def authorization(self):
-      # return "Basic %s:%s" % (self.access_key_id(), self.secret_access_key())
-      # return "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vaWFtLmxvY2FsaG9zdDozMDAwIiwic3ViIjoidXJuOndoaXN0bGVyOmlhbTo6MjIyMjIyMjIyOnVzZXIvQWRtaW5fMiIsInNjb3BlIjoiKiIsImF1ZCI6WyJodHRwOi8vbG9jYWxob3N0OjMwMDAiXSwiaWF0IjoxNTY3NTc1MTI1fQ.I7y4FLu1gk5lNTwesp52SvKD4FuzxJcYtCMKj4V6nLM"
-      # return "Basic AEPEMJNUOYLNMPINPXLQ:KWfCzUehJJBfJRPcY2F6LbCugCwSVSsG1SnG0uqcpc6cLISQ7EQttg"
-      return "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2lhbS5hcGkud2hpc3RsZXIucGVyeHRlY2gub3JnIiwic3ViIjoidXJuOnBlcng6aWFtOjoyMjIyMjIyMjI6dXNlci9BZG1pbl8yIiwic2NvcGUiOiIqIiwiYXVkIjpbImh0dHBzOi8vYXBpLndoaXN0bGVyLnBlcnh0ZWNoLm9yZyJdLCJpYXQiOjE1Njc4MzY5MjB9.fyCT7D74FMNhFpuUJAn9Nxpxu3pMLBJ5bsHMw_L_bQE"
+  def authorization(self):
+    # return "Basic %s:%s" % (self.access_key_id(), self.secret_access_key())
+    # return "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vaWFtLmxvY2FsaG9zdDozMDAwIiwic3ViIjoidXJuOndoaXN0bGVyOmlhbTo6MjIyMjIyMjIyOnVzZXIvQWRtaW5fMiIsInNjb3BlIjoiKiIsImF1ZCI6WyJodHRwOi8vbG9jYWxob3N0OjMwMDAiXSwiaWF0IjoxNTY3NTc1MTI1fQ.I7y4FLu1gk5lNTwesp52SvKD4FuzxJcYtCMKj4V6nLM"
+    return "Basic ADHWNYMMJMNLNEFNFMJU:90SUtEj9hxiKzl6O_aX_YH6lGIRJwVkXo2y_TTp9w4LlbV_lFXhzlQ"
+    # return "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2lhbS5hcGkud2hpc3RsZXIucGVyeHRlY2gub3JnIiwic3ViIjoidXJuOnBlcng6aWFtOjoyMjIyMjIyMjI6dXNlci9BZG1pbl8yIiwic2NvcGUiOiIqIiwiYXVkIjpbImh0dHBzOi8vYXBpLndoaXN0bGVyLnBlcnh0ZWNoLm9yZyJdLCJpYXQiOjE1Njc4MzY5MjB9.fyCT7D74FMNhFpuUJAn9Nxpxu3pMLBJ5bsHMw_L_bQE"
 
-    @task(4)
-    def get_iam_users(self):
-        self.client.get('/iam/users', headers={"authorization": self.authorization()})
+  @task(2)
+  def get_iam_users(self):
+    self.client.get('/iam/users', headers={"authorization": self.authorization()})
 
-    @task(2)
-    def get_cognito_users(self):
-        self.client.get('/cognito/users', headers={"authorization": self.authorization()})
+  @task(2)
+  def get_cognito_users(self):
+    self.client.get('/cognito/users', headers={"authorization": self.authorization()})
 
-    @task(2)
-    def cognito_login(self):
-        self.client.post('/cognito/login', { "data": { "attributes": { "primary_identifier": "Mariaidentifier" } } }, headers={"authorization": self.authorization()})
+  @task(2)
+  def cognito_login(self):
+    payload = { "data": { "attributes": { "primary_identifier": "miller" } } }
+    self.client.post('/cognito/login', data=json.dumps(payload), headers={ "authorization": self.authorization(), 'content-type': 'application/vnd.api+json' } )
+
+  @task(2)
+  def create_possible_outcomes(self):
+    payload = { 'data': { "type": "possible_outcomes", "attributes": { "result_id": 1, "result_type": "Perx::Reward::Entity", "campaign_entity_id": 1 } } }
+    self.client.post('/outcome/possible_outcomes', data=json.dumps(payload), headers={ "authorization": self.authorization(), 'content-type': 'application/vnd.api+json' } )
 
   #  @task
   #  def shake(self):
@@ -41,8 +47,9 @@ class IamTests(TaskSet):
 
 
 class IamService(HttpLocust):
-    task_set = IamTests
-    # host = 'http://localhost:3000'
-    host = 'https://api.whistler.perxtech.org'
-    # max_weight =
-    # min_weight =
+  task_set = IamTests
+  host = 'http://localhost:3000'
+  # 
+  # host = 'https://api.whistler.perxtech.org'
+  # max_weight =
+  # min_weight =
