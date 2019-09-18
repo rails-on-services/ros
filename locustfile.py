9c80e519c6ffdbb5c0b1c359200a5b8338f49db1
Shi Hao <shihaotan91@gmail.com>
2019-09-18 14:56:37 +0800

    finish up loadtest

--- locustfile.py
+++ locustfile.py
@@ -5,23 +5,25 @@ import pdb
 from locust import HttpLocust, task, TaskSet, TaskSequence
 from faker import Faker
 
-FILE = 'tmp/runtime/development/be/application/mounted/platform/credentials.json'
+FILE = 'services/iam/tmp/mounted/credentials.json'
 
-# def config():
-#   return json.loads(open(FILE).read())
-
-# def access_key_id(self):
-#   return self.config()[2]['credential']['access_key_id']
-
-# def secret_access_key(self):
-#   return self.config()[2]['secret']  
+def config():
+  return json.loads(open(FILE).read())
 
 def authorization():
-  return "Basic AGKQQTPKMZQYQJIMIAMM:Li3hJQOXrwtxp_ytIXM8jv2w7d_OX62CBQ4O9d-SOTSKUO3Egw6MfA"
+  access_key_id = config()[2]['credential']['access_key_id']
+  secret_access_key = config()[2]['secret']
+  return 'Basic %s:%s' %(access_key_id, secret_access_key)
 
-# def create_iam_user(self):
-#   payload =  { "data": { "type": "users", "attributes": { "username": Faker().name(), "time_zone": "SGT" } } }
-#   self.client.post('/iam/users', data=json.dumps(payload), headers={"authorization": authorization(), 'content-type': 'application/vnd.api+json'})  
+def access_key_id(self):
+  return self.config()[2]['credential']['access_key_id']
+
+def secret_access_key(self):
+  return self.config()[2]['secret']
+
+def create_iam_user(self):
+  payload =  { "data": { "type": "users", "attributes": { "username": Faker().name(), "time_zone": "SGT" } } }
+  self.client.post('/iam/users', data=json.dumps(payload), headers={"authorization": authorization(), 'content-type': 'application/vnd.api+json'})  
 
 def create_cognito_user(self, identifier):
   payload =  { "data": { "type": "users", "attributes": { "primary_identifier": identifier } } }
@@ -33,6 +35,8 @@ def login_cognito_user(self, identifier):
 
 class SurveyEngagement(TaskSet):
   def setup(self):
+    create_iam_user(self)
+
     identifier = Faker().name()
     create_cognito_user(self, identifier)
     login_response = login_cognito_user(self, identifier)
@@ -40,25 +44,24 @@ class SurveyEngagement(TaskSet):
     self.token = login_response.headers['Authorization']
     self.content_type = 'application/vnd.api+json'
 
-  def on_start(self):
-    pdb.set_trace()
+  def on_start(self):  
     engagement_response = self.create_survey_engagement()
     self.engagement_id = json.loads(engagement_response.content)['data']['id']
-
-    batch_response = self.create_voucher_batch()
-    self.batch_id = json.loads(batch_response.content)['data'][0]['id']
-
-    campaign_response = self.create_campaign_entity()
-    self.campaign_id = json.loads(campaign_response.content)['data']['id']
 
     organization_response = self.create_organization_orgs()
     self.org_id = json.loads(organization_response.content)['data']['id']
 
+    campaign_response = self.create_campaign_entity()
+    self.campaign_id = json.loads(campaign_response.content)['data']['id']
+
     reward_response = self.create_reward_entity()
     self.reward_id = json.loads(reward_response.content)['data']['id']
 
+    # batch_response = self.create_voucher_batch()
+    # self.batch_id = json.loads(batch_response.content)['data'][0]['id']
+
   def create_voucher_batch(self):
-    payload = { "data": { "type": "batch", "attributes": { "amount": 50, "start_date": "2019-01-01", "source_id": 1, "source_type": "reward", "code_type": "single_code", "code": "12345" } } }
+    payload = { "data": { "type": "batch", "attributes": { "amount": 50, "start_date": "2019-01-01", "source_id": self.reward_id, "source_type": "Perx::Reward::Entity", "code_type": "single_code", "code": "100" } } }
     return self.client.post('/voucher/batch', data=json.dumps(payload), headers={ "authorization": self.token, 'content-type': self.content_type } )
 
   def create_survey_engagement(self):
@@ -93,22 +96,24 @@ class SurveyEngagement(TaskSet):
     return self.client.post('/organization/orgs', data=json.dumps(payload), headers={ "authorization": self.token, 'content-type': self.content_type } )
 
   def create_reward_entity(self):
-    payload = { "data": { "type": "entities", "attributes": { "name": "Starbucks Voucher", "reward_type": "Perx::Reward::Entity", "category": "Perx::Reward::Entity", "redemption_type": "Text", "cost_of_reward": 200, "organization_id": self.org_id } } }   
+    payload = { "data": { "type": "entities", "attributes": { "image_url": "https://lorempixel.com/300/300", "name": "Starbucks Voucher", "reward_type": "Cashback", "category": "F&B", "redemption_type": "Text", "cost_of_reward": 200, "organization_id": self.org_id } } }   
     return self.client.post('/reward/entities', data=json.dumps(payload), headers={ "authorization": self.token, 'content-type': self.content_type } )
 
-  @task(5)
-  def create_voucher_entities(self):
-    payload = { 'data': { "type": "entities", "attributes": { "batch_id": self.batch_id, "source_id": self.reward_id, "source_type": "Perx::Reward::Entity", "campaign_entity_id": 1, "user_id": "1" } } }
-    self.client.post('/voucher/entities', data=json.dumps(payload), headers={ "authorization": self.token, 'content-type': self.content_type } )
+  # @task(20)
+  # NOTE: This endpoint is not yet restful. Uncomment after the create method on voucher/entities_controller have been refactored/removed
+  # def create_voucher_entities(self):
+  #   payload = { 'data': { "type": "entities", "attributes": { "batch_id": self.batch_id, "source_id": self.reward_id, "source_type": "Perx::Reward::Entity", "user_id": "1" } } }
+  #   response = self.client.post('/voucher/entities', data=json.dumps(payload), headers={ "authorization": self.token, 'content-type': self.content_type } )
 
-  @task(5)
+  @task(20)
   def create_possible_outcomes(self):
-    payload = { 'data': { "type": "possible_outcomes", "attributes": { "result_id": 1, "result_type": "Perx::Reward::Entity", "campaign_entity_id": self.campaign_id } } }
+    pdb.set_trace
+    payload = { 'data': { "type": "possible_outcomes", "attributes": { "result_id": self.reward_id, "result_type": "Perx::Reward::Entity", "campaign_entity_id": self.campaign_id } } }
     self.client.post('/outcome/possible_outcomes', data=json.dumps(payload), headers={ "authorization": self.token, 'content-type': self.content_type } )
 
-  @task(5)
+  @task(20)
   def create_survey_answers(self):
-    payload = { "data": { "type": "answers", "attributes": { "engagement_id": self.engagement_id, "campaign_entity_id": self.campaign_id, "content": { "something": "good" } } } }
+    payload = { "data": { "type": "answers", "attributes": { "engagement_id": 1, "campaign_entity_id": 1, "content": { "something": "good" } } } }
     self.client.post('/survey/answers', data=json.dumps(payload), headers={ "authorization": self.token, 'content-type': self.content_type } )  
 
 class SurveyEngagementService(HttpLocust):
