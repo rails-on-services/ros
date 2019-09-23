9f6459cf1b695d5e81fa6db769b95e216705311a
Shi Hao <shihaotan91@gmail.com>
2019-09-23 18:08:33 +0800

    update request specs template to cover show specs

--- lib/core/lib/generators/specs_generator.rb
+++ lib/core/lib/generators/specs_generator.rb
@@ -8,6 +8,39 @@ module SpecsGenerator
       require 'rails_helper'
 
       RSpec.describe "#{plural_name} requests", type: :request do
+        let(:tenant) { Tenant.first }
+        let(:mock) { true }
+        let(:url) { "/#{plural_name}" }
+
+        context 'all' do
+          include_context 'jsonapi requests'
+
+          describe 'GET index' do
+            let(:models_count) { rand(5) }
+            # Doesn't work yet, figure out how to create new records through factory using variables
+            # let(:models) { create_list("#{name.to_sym}", models_count) }
+
+            context 'Unauthenticated user' do
+              include_context 'unauthorized user'
+              include_examples 'unauthenticated get'
+            end
+
+            context 'authenticated user' do
+              include_context 'authorized user'
+
+              before do
+                mock_authentication if mock
+                get url, headers: request_headers
+              end
+
+              it 'returns a successful response' do
+                expect(response).to have_http_status(:ok)
+                # Doesn't work yet
+                # expect_json_sizes('data', models_count)
+              end
+            end
+          end
+        end
       end
     FILE
   end
