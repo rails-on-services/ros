f4f12c8473c6f36a93cec9de7e26cca4962221ec
Sergey Toy <sergeytoy@perxtech.com>
2019-09-23 15:08:31 +0700

    WIP. Initial example groups for models

---
+++ lib/core/spec/shared/models/example_groups.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'application record concern' do
+
+  let(:partition_name) { Settings.partition_name }
+  let(:service_name) { Settings.service.name }
+  let(:region) { Settings.region }
+  let(:account_id) { described_class.account_id }
+  let(:underscore_name) { described_class.name.underscore }
+  let(:urn) { "urn:#{partition_name}:#{service_name}:#{region}:#{account_id}:#{underscore_name}" }
+
+  let(:factory_name) { described_class.name.downcase.to_sym }
+  
+  it 'has a factory' do
+    expect { create(factory_name) }.not_to raise_error(KeyError)
+  end
+
+  context 'class methods' do
+    before do
+      tenant.switch!
+    end
+
+    it 'respind to name and its is not nil' do
+      expect(described_class.respond_to?(:name)).to be_truthy
+      expect(described_class.name).not_to be_nil
+    end
+
+    it 'respind to urn_base and its is not nil' do
+      expect(described_class.respond_to?(:urn_base)).to be_truthy
+      expect(described_class.urn_base).not_to be_nil
+    end
+
+    it 'related to current tennant' do
+      expect(described_class.current_tenant).to eq(tenant)
+    end
+
+    it 'respond to to_urn and its valid' do
+      expect(described_class.respond_to?(:to_urn)).to be_truthy
+      expect(described_class.to_urn).to eq(urn)
+    end
+  end
+
+  context 'instance methods' do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+end
