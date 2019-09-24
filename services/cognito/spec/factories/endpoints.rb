84f99ccaec0f31a8888fc9ca591e48cc8213b1b2
Sergey Toy <sergeytoy@perxtech.com>
2019-09-24 12:48:33 +0700

    remove tenant relation from endpoint factory

--- services/cognito/spec/factories/endpoints.rb
+++ services/cognito/spec/factories/endpoints.rb
@@ -3,7 +3,6 @@
 FactoryBot.define do
   factory :endpoint do
     url { 'http://localhost:3000/test' }
-    tenant
     target_type { 'Survey::Campaign' }
     target_id { 1 }
     properties { '' }
