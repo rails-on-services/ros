238dbb37a6c2933bc37ddcdec3c61b44acea8d04
Sergey Toy <sergeytoy@perxtech.com>
2019-09-25 14:54:04 +0700

    seeding six tenants

--- services/cognito/db/seeds/development/tenants.seeds.rb
+++ services/cognito/db/seeds/development/tenants.seeds.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-# rubocop:disable UselessAssignment
-start_id = (Tenant.last&.id || 0) + 1
-(start_id..start_id + 1).each do |id|
-  is_even = (id % 2).zero?
+(1..6).each do |id|
   Tenant.create!(schema_name: Tenant.account_id_to_schema(id.to_s * 9))
 end
-# rubocop:enable UselessAssignment
--- services/iam/db/seeds/development/tenants.seeds.rb
+++ services/iam/db/seeds/development/tenants.seeds.rb
@@ -5,22 +5,29 @@ initialize = Root.find_by(email: 'root@platform.com').nil?
 
 if initialize
   create_list = [
-    { email: 'root@platform.com', password: 'asdfjkl;', api: true },
-    { email: 'root@client2.com', password: 'asdfjkl;' }
+    { email: 'root@platform.com', password: 'asdfjkl;', api: true, alias: 'perx' },
+    { email: 'root@generic.com', password: 'asdfjkl;', alias: 'generic' },
+    { email: 'root@banking.com', password: 'asdfjkl;', alias: 'banking' },
+    { email: 'root@telco.com', password: 'asdfjkl;', alias: 'telco' },
+    { email: 'root@insurance.com', password: 'asdfjkl;', alias: 'insurance' },
+    { email: 'root@retail.com', password: 'asdfjkl;', alias: 'retail' }
   ]
 else
-  id = Root.last.id
-  create_list = [
-    { email: "root@client#{id + 1}.com", password: 'asdfjkl;' },
-    { email: "root@client#{id + 2}.com", password: 'asdfjkl;' }
-  ]
+  # TODO: We don't need it anymore
+  # id = Root.last.id
+  # create_list = [
+  #   { email: "root@client#{id + 1}.com", password: 'asdfjkl;' },
+  #   { email: "root@client#{id + 2}.com", password: 'asdfjkl;' }
+  # ]
+  create_list = []
 end
 
 @created_list = []
 create_list.each do |account|
+  tenant_alias = account.delete(:alias)
   Root.create!(account).tap do |root|
     root.create_tenant(schema_name: Tenant.account_id_to_schema(root.id.to_s * 9)[0..10],
-                       name: "Account #{id}", state: :active)
+                       name: "Account #{root.id}", state: :active, alias: tenant_alias)
     credential = Credential.create(owner: root)
     @created_list.append(type: 'root', owner: root, tenant: root.tenant, credential: credential,
                          secret: credential.secret_access_key)
