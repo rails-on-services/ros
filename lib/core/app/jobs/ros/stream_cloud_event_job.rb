c9ef2e7dd95cdac3441c5bd4c04192074388237f
Shi Hao <shihaotan91@gmail.com>
2019-09-26 10:47:20 +0800

    change object to hash with as json

--- lib/core/app/jobs/ros/stream_cloud_event_job.rb
+++ lib/core/app/jobs/ros/stream_cloud_event_job.rb
@@ -10,7 +10,7 @@ module Ros
     def perform(object)
       type = "#{Settings.service.name}.#{object.class.name.downcase}"
       message_id = object.id # SecureRandom.uuid
-      data = object.to_json
+      data = object.as_json
       # data = { id: object.id, username: object.username, time_zone: object.time_zone }.to_json
       Rails.configuration.x.event_logger.log_event(type, message_id, data)
     end
