0d8de78dc3a0a1bc860079323417d9d32fe2cc70
Sergey Toy <toydestroyer@users.noreply.github.com>
2019-09-27 10:27:38 +0700

    [PW-1270] Implement filtering organization by name, with partial matching (#74)
    # Describe the changes made. What does this PR changes that might be critical. If any critical decisions have been made, make sure you explain the rationale for these decisions.
    
    - Add filter to `orgs` resource on `organization` service. Filter with partial matching and case insensitive.
    
    # How does the implementation addresses the problem
    We're able to filter organizations by name like:
    `GET /organization/orgs?filter%5Bname%5D=string`

--- services/organization/app/resources/org_resource.rb
+++ services/organization/app/resources/org_resource.rb
@@ -4,4 +4,6 @@ class OrgResource < Organization::ApplicationResource
   attributes :name, :description, :properties
 
   has_many :branches
+
+  filter :name, apply: ->(records, value, _options) { records.where('name ILIKE ?', "%#{value[0]}%") }
 end
