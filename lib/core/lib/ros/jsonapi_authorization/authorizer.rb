b41bcb675a3f72963d3a70ad82da55a7b25e5625
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-09-30 17:49:26 +0800

    WIP - working in has_one relationships

--- lib/core/lib/ros/jsonapi_authorization/authorizer.rb
+++ lib/core/lib/ros/jsonapi_authorization/authorizer.rb
@@ -3,12 +3,18 @@
 module Ros
   module JsonapiAuthorization
     class Authorizer < JSONAPI::Authorization::DefaultPunditAuthorizer
-      def find(source_class:)
+      def include_has_many_resource(source_record:, record_class:)
         binding.pry
         super
       end
 
-      def show(source_record:)
+      def include_has_one_resource(source_record:, related_record:)
+        binding.pry
+        source_class = source_record.class
+        inner_associations = source_class.reflect_on_all_associations(:belongs_to).map(&:class_name)
+        inner_associations += source_class.reflect_on_all_associations(:has_one).map(&:class_name)
+        return unless inner_associations.include? related_record.class.name
+
         binding.pry
         super
       end
