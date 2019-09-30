59a57590a97a5202f3db2d36c53501d0ac80f16e
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-09-30 18:13:52 +0800

    WIP - handling has many resource

--- lib/core/lib/ros/jsonapi_authorization/authorizer.rb
+++ lib/core/lib/ros/jsonapi_authorization/authorizer.rb
@@ -4,6 +4,10 @@ module Ros
   module JsonapiAuthorization
     class Authorizer < JSONAPI::Authorization::DefaultPunditAuthorizer
       def include_has_many_resource(source_record:, record_class:)
+        source_class = source_record.class
+        inner_associations = source_class.reflect_on_all_associations(:has_many).map(&:class_name)
+        return unless inner_associations.include? related_record.class.name
+
         binding.pry
         super
       end
