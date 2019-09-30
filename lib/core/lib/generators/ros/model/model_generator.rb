b609b50da0b74b60052701ed3caef982c4d7fee3
Shi Hao <shihaotan91@gmail.com>
2019-09-30 12:01:41 +0800

    fix rubocop error by making create_files methods into smaller chuncks

--- lib/core/lib/generators/ros/model/model_generator.rb
+++ lib/core/lib/generators/ros/model/model_generator.rb
@@ -9,12 +9,21 @@ module Ros
     def create_files
       invoke(:model)
 
+      gsub_created_file
+      insert_spec_default_template_into_file
+    end
+
+    private
+
+    def gsub_created_file
       gsub_file(
         "app/models/#{name}.rb",
         'ApplicationRecord',
         "#{parent_module}ApplicationRecord"
       )
+    end
 
+    def insert_spec_default_template_into_file
       insert_into_file "spec/models/#{name}_spec.rb", after: ":model do\n" do
         "  include_examples 'application record concern' do\n"\
         "    let(:tenant) { Tenant.first }\n"\
