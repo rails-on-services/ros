83e66d26d661df5afc29de1909885f8b7fc66e7f
Alexey <alexeyn.lightit@gmail.com>
2019-10-03 13:15:12 +0300

    fixed returned resource type added resource links :self

--- lib/core/app/models/concerns/association_resource/included_resource.rb
+++ lib/core/app/models/concerns/association_resource/included_resource.rb
@@ -6,6 +6,7 @@
 module AssociationResource
   class IncludedResource < Ros::ApplicationResource
     immutable
+    cattr_accessor :sub_name
 
     def initialize(model, context)
       reload_attributes!
@@ -15,18 +16,38 @@ module AssociationResource
       super
     end
 
-    # TODO: Would be nice that this generates the link to the remote
-    # service that the class is connecting to.
-    exclude_links [:self]
+    def custom_links(_options)
+      {
+        self: _model&.url
+      }
+    end
 
     def self._model_class
       _type
     end
 
+    def included?
+      true
+    end
+
+    # While each resource class created dynamically from Class.new
+    # And resource type is created from class name after inherited callback
+    # And inherited callback is called before class initialization
+    # we have to set name for Class.new (Class.new(Parent).name => nil)
+    # and extract Class.name from this class attribute
+    def self.inherited(subclass)
+      subclass.instance_variable_set(:@name, sub_name) if sub_name.present?
+      super
+    end
+
+    def self.name
+      instance_variable_get(:@name) || super
+    end
+
     private
 
     def extract_attributes(model)
-      model.attributes.except(:type, :id).symbolize_keys.keys.compact
+      model.attributes.except(:id, :type).symbolize_keys.keys.compact
     end
 
     def reload_attributes!
