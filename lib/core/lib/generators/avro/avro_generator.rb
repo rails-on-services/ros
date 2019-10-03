10e1be12f44e47be2c5a530bdbea5b2a9be3c804
Shi Hao <shihaotan91@gmail.com>
2019-10-03 12:53:19 +0800

    remove dictionary from avro generator

--- lib/core/lib/generators/avro/avro_generator.rb
+++ lib/core/lib/generators/avro/avro_generator.rb
@@ -3,20 +3,13 @@
 require_relative './avro_builder.rb'
 
 class AvroGenerator < Rails::Generators::Base
-  DICTIONARY = {
-    integer: 'int',
-    string: 'string',
-    datetime: 'string',
-    jsonb: 'string'
-  }.freeze
-
   def create_files
     Ros.table_names.each do |name|
       next unless model_defined?(name.classify)
 
       service_name ||= Settings.service.name
       singular_name = name.singularize
-
+      
       create_file "doc/schemas/cloud_events/#{service_name}/#{singular_name}.avsc" do
         avro_builder = AvroBuilder.new(singular_name, service_name)
         JSON.pretty_generate avro_builder.build_model_attributes
