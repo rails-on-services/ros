483f779acafcb00e81c29eb6a68603ff58adbfae
Shi Hao <shihaotan91@gmail.com>
2019-10-03 18:16:05 +0800

    remove unncessary files and commented out code

--- lib/core/app/models/cloud_event_subject.rb
+++ lib/core/app/models/cloud_event_subject.rb
@@ -17,17 +17,4 @@ class CloudEventSubject
   def persisted?
     false
   end
-
-  # Not sure if these are important now
-  # def self.all
-  #   new
-  # end
-
-  # def order
-  #   [self] # following order collect is called on the result so return an array with self
-  # end
-
-  # def count
-  #   1 # count is called for meta
-  # end
 end
