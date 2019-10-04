5a115df6a9c7250e9cae20463cd05c53380e14b1
shihaotan91 <shihaotan91@gmail.com>
2019-10-04 15:40:45 +0800

    [PW-1314] Avro generator (#79)
    [Avro generator](https://perxtechnologies.atlassian.net/browse/PW-1314)
    
    # Describe the changes made. What does this PR changes that might be critical.
    # If any critical decisions have been made, make sure you explain the rationale
    # for these decisions.
    
    - write generator to create avro files to feed into cloud events

---
+++ lib/core/lib/generators/avro/avro_generator.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative './avro_builder.rb'
+
+class AvroGenerator < Rails::Generators::NamedBase
+  def create_files
+    return unless model_defined?(name.classify)
+
+    service_name = Settings.service.name
+
+    create_file "#{Settings.event_logging.config.schemas_path}/#{service_name}/#{name}.avsc" do
+      avro_builder = AvroBuilder.new(name, service_name)
+      JSON.pretty_generate avro_builder.build_model_attributes
+    end
+  end
+
+  private
+
+  def model_defined?(name)
+    Object.const_defined?(name)
+  end
+end
