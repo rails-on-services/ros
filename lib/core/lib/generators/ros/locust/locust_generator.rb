cb97a5e388b4590b18b750a55ae9de6420545199
Shi Hao <shihaotan91@gmail.com>
2019-10-08 17:30:53 +0800

    extract code to a erb file

--- lib/core/lib/generators/ros/locust/locust_generator.rb
+++ lib/core/lib/generators/ros/locust/locust_generator.rb
@@ -3,51 +3,27 @@
 module Ros
   class LocustGenerator < Rails::Generators::NamedBase
     def create_files
-      create_file "../../lib/sre/lib/#{Settings.service.name}/#{name}.py", <<~FILE
-        import json
-        import pdb
-        from locust import HttpLocust, task, TaskSet, TaskSequence
+      source = 'ros/lib/core/lib/template/locust.yml.erb'
+      destination = "../../lib/sre/lib/#{Settings.service.name}/#{name}.py"
 
-        from base import login
-
-        class #{class_name}(TaskSet):
-          def on_start(self):
-            login.setup(self)
-
-            self.get_all_#{Settings.service.name}_#{plural_name}()
-            self.get_#{Settings.service.name}_#{name}(1)
-
-          @task(2)
-          def get_all_#{Settings.service.name}_#{plural_name}(self):
-            self.client.get('#{Settings.service.name}/#{plural_name}', headers=self.header )
-
-          @task(2)
-          def get_#{Settings.service.name}_#{name}(self, id):
-            path = ('#{Settings.service.name}/#{plural_name}/%s' %(id))
-            self.client.get(path, headers=self.header )
-
-          @task(2)
-          def create_#{Settings.service.name}_#{name}(self, #{create_args}):
-            payload = #{payload}
-            self.client.get('#{Settings.service.name}/#{plural_name}', data=json.dumps(payload), headers=self.header )
-
-        class #{class_name}Service(HttpLocust):
-          task_set = #{class_name}
-          # NOTE: Running it locally
-          host = 'http://localhost:3000/'
-          # NOTE: Running it on uat environment with load-test tag
-          # host = 'https://api-load-test.uat.whistler.perxtech.io/'
-          max_weight = 500
-          min_weight = 500
-      FILE
+      template(source, destination, values)
     end
 
     private
 
+    def source_paths
+      [ Rails.root.join('../..') ]
+    end
+
     def values
-      OpenStruct.new({
-        name: Settings.service.name
-      })
+      OpenStruct.new(
+        service_name: Settings.service.name,
+        class_name: class_name,
+        name: name,
+        plural_name: plural_name,
+        extra_args: extra_args,
+        payload: payload
+      )
     end
 
     def class_name
@@ -63,7 +39,7 @@ module Ros
       model.attributes.except(*args)
     end
 
-    def create_args
+    def extra_args
       associations_attributes = model.attributes.select { |attribute| attribute.end_with?('_id') }
       associations_attributes.keys.join(', ')
     end
