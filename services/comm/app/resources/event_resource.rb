27741fc9614fb80d70c3cc7986651ca101b53bab
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-10-10 16:37:17 +0800

    WIP - allowing pool id to be passed

--- services/comm/app/resources/event_resource.rb
+++ services/comm/app/resources/event_resource.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
 class EventResource < Comm::ApplicationResource
-  attributes :name, :send_at, :provider_id, :campaign_entity_id, :template_id, :channel
+  attributes :name, :send_at, :provider_id, :campaign_entity_id, :template_id, :channel, :pool_id
   attributes :target_type, :target_id
 
   has_one :provider
   has_one :template
 
   filter :campaign_entity_id
+
+  def fetchable_fields
+    super - %i[pool_id]
+  end
 
   def pool_id=(pool_id)
     @model.target_type = 'Ros::Cognito::Pool'
