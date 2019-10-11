6d105aa12fc77832009cf15fe74eaa88deab359b
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-10-11 13:03:32 +0800

    WIP - added resource params

--- services/comm/app/resources/event_resource.rb
+++ services/comm/app/resources/event_resource.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class EventResource < Comm::ApplicationResource
-  attributes :name, :send_at, :provider_id, :campaign_entity_id, :template_id, :channel, :pool_id
-  attributes :target_type, :target_id
+  attributes :name, :send_at, :provider_id, :campaign_id, :template_id, :channel, :pool_id
+  attributes :target_type, :target_id, :owner_type, :owner_id
 
   has_one :provider
   has_one :template
 
-  filter :campaign_entity_id
+  filters :owner_type, :owner_id
 
   def fetchable_fields
     super - %i[pool_id]
