1b7172044e4bf77496711f0c179cfeb70da9ccfc
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-10-11 13:30:22 +0800

    WIP - removed cid from template

--- services/comm/app/resources/template_resource.rb
+++ services/comm/app/resources/template_resource.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class TemplateResource < Comm::ApplicationResource
-  attributes(:name, :description, :content, :status, :campaign_entity_id)
+  attributes(:name, :description, :content, :status)
   # has_one :campaign
-
-  filter :campaign_entity_id
 end
