e370bfd2669610eb349b7a7af2bdfb07b6d8016b
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-10-11 15:08:38 +0800

    WIP - updating tenant factories properties

--- lib/core/spec/factories/tenants.rb
+++ lib/core/spec/factories/tenants.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :tenant do
     schema_name { rand(100_000_000..999_999_999).to_s.scan(/.{3}/).join('_') }
-    properties { '' }
-    platform_properties { '' }
+    properties { {} }
+    platform_properties { {} }
   end
 end
