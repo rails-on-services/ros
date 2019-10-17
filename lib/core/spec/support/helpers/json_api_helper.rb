92293624318475cb91b840ed88d3b4c7bb751630
Alexey <alexeyn.lightit@gmail.com>
2019-10-17 19:27:44 +0800

    extended jsonapi_data rspec helper with relationships

--- lib/core/spec/support/helpers/json_api_helper.rb
+++ lib/core/spec/support/helpers/json_api_helper.rb
@@ -5,6 +5,7 @@ module JsonApiSpecHelper
     extra_attributes = options.fetch(:extra_attributes, {})
     skip_attributes = options.fetch(:skip_attributes, [])
     method = options.fetch(:method, :get)
+    relations = Array.wrap(options[:relationships])
 
     generic_attributes = %i[id created_at updated_at]
     skip_attributes.append(*generic_attributes)
@@ -14,6 +15,22 @@ module JsonApiSpecHelper
       attributes: object.attributes.except(*skip_attributes.map(&:to_s)).merge(extra_attributes)
     }
     data[:id] = object.id if %i[put patch].include?(method.downcase.to_sym)
+    relation_hash = {}
+    relations.each do |relation|
+      name = relation[:name]
+      type = relation[:type] || name.to_s.pluralize
+      id = relation[:id]
+      next unless id && type
+
+      relation_hash[name] = {
+        data: {
+          type: type,
+          id: id
+        }
+      }
+    end
+
+    data[:relationships] = relation_hash if relation_hash.present?
 
     { data: data }.to_json
   end
