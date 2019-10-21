b7f97fbc37ea9b39e59afaddafd46700cd0e40b1
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-10-21 23:56:46 +0800

    WIP - cleanup code

--- services/comm/app/operations/event_process.rb
+++ services/comm/app/operations/event_process.rb
@@ -3,30 +3,28 @@
 # NOTE: Might be more worthy to implement this using TRB operation instead
 class EventProcess < Trailblazer::Activity::FastTrack
   # rubocop:disable Style/SignalException
-  step :find_event
+  step :find_event, input: [:params], output: %i[event template campaign]
   fail :event_not_found, fail_fast: true
-  step :setup_context
   step :process_event
   # rubocop:enable Style/SignalException
 
   def find_event(ctx, params)
-    ctx[:model] = ::Event.find_by(params)
+    event = ::Event.find_by(params)
+    return false unless event
+
+    ctx[:event] = event
+    ctx[:template] = event.template
+    ctx[:campaign] = event.campaign
   end
 
-  def event_not_found(_ctx, params)
+  def event_not_found(_ctx)
     ctx[:errors] << "{EventProcess} Can't find event (#{params}) for tenant"
   end
 
-  def setup_context(ctx, _params)
-    ctx[:template] = ctx[:model].template
-    ctx[:campaign] = ctx[:model].campaign
-  end
-
-  def process_event(ctx, params)
-    event = ctx[:model]
+  def process_event(_ctx, event:, template:, campaign:)
     event.process!
     event.users.each do |user|
-      content = ctx[:template].render(user, ctx[:campaign])
+      content = template.render(user, campaign)
       event.messages.create(provider: event.provider, channel: event.channel, to: user.phone_number, body: content)
     end
     event.publish!
