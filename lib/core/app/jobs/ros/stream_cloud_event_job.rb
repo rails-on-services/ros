4bf8755393006e34fabe9381404161545b4b1588
Sergey Toy <toydestroyer@users.noreply.github.com>
2019-10-21 16:08:46 +0800

    [PW-1847] Enqueue cloudevent after_commit hook in sidekiq
    [Enqueue cloudevent after_commit hook in sidekiq](https://perxtechnologies.atlassian.net/browse/PW-1847)
    
    # Describe the changes made. What does this PR changes that might be critical. If any critical decisions have been made, make sure you explain the rationale for these decisions.
    Currently after commit jobs for cloudevent enqueued with `perform_now`. In this PR we replace it with `perform_later`.

--- lib/core/app/jobs/ros/stream_cloud_event_job.rb
+++ lib/core/app/jobs/ros/stream_cloud_event_job.rb
@@ -5,13 +5,9 @@ module Ros
   # If any event listeners have been configured for this object+event combination then
   # enque a PlatformConsumerEventJob on the listening service's platform_consumer_events queue
   class StreamCloudEventJob < Ros::ApplicationJob
-    queue_as "#{Settings.service.name}_stream_cloud_event"
+    queue_as "#{Settings.service.name}_platform_producer_events"
 
-    def perform(object)
-      type = "#{Settings.service.name}.#{object.class.name.downcase}"
-      message_id = object.id # SecureRandom.uuid
-      data = object.as_json
-      # data = { id: object.id, username: object.username, time_zone: object.time_zone }.to_json
+    def perform(type, message_id, data)
       Rails.configuration.x.event_logger.log_event(type, message_id, data)
     end
   end
