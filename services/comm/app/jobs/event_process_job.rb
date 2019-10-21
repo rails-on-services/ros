b952d1eb804919d34ff7cffb0482d022a30c4831
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-10-21 20:35:59 +0800

    WIP - removed all old logic

--- services/comm/app/jobs/event_process_job.rb
+++ services/comm/app/jobs/event_process_job.rb
@@ -1,34 +1,4 @@
 # frozen_string_literal: true
 
 class EventProcessJob < Comm::ApplicationJob
-  # MessagesController receives a POST request to create a message (sms) with details of from, to and body
-  # After the record is created, a Job is created to send to the destination
-  # This means that the correct tenant must be selected by apartment
-  # def perform(*args)
-    # event = Event.find(event_id)
-    # if event
-    #   process_event(event)
-    # else
-    #   Rails.logger.info "{EventJob} Can't find event (#{event_id}) for tenant (#{tenant.schema_name})"
-    # end
-  # end
-
-  private
-
-  def process_event(event)
-    event.process!
-    event.users.each do |user|
-      content = template.render(user, campaign)
-      event.messages.create(provider: event.provider, channel: event.channel, to: user.phone_number, body: content)
-    end
-    event.publish!
-  end
-
-  def template
-    @template ||= @event.template
-  end
-
-  def campaign
-    @campaign ||= @event.campaign
-  end
 end
