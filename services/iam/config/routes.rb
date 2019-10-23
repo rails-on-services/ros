b0706a95e60be62f3fb38ebbba98ca4201f776d2
Peter Hoeg <peter@perxtech.com>
2019-10-23 18:14:18 +0800

    cleanup: sorting

--- services/iam/config/routes.rb
+++ services/iam/config/routes.rb
@@ -4,9 +4,9 @@ Ros::Iam::Engine.routes.draw do
   devise_for :users, module: 'users', defaults: { format: :json }
   devise_for :roots, module: 'roots', defaults: { format: :json }
 
-  jsonapi_resources :roots
-  jsonapi_resources :users
   jsonapi_resources :credentials
   jsonapi_resources :groups
   jsonapi_resources :policies
+  jsonapi_resources :roots
+  jsonapi_resources :users
 end
