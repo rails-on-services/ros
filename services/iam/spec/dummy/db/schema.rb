ce26176134caad955e03455c088ea5d2ba9250a2
Peter Hoeg <peter@perxtech.com>
2019-10-23 12:45:29 +0800

    iam: Add email and confirmable

--- services/iam/spec/dummy/db/schema.rb
+++ services/iam/spec/dummy/db/schema.rb
@@ -168,12 +168,18 @@ ActiveRecord::Schema.define(version: 2019_09_24_091536) do
     t.jsonb "properties", default: {}, null: false, comment: "Custom properties of the user"
     t.jsonb "display_properties", default: {}, null: false, comment: "Custom display properties of the user"
     t.string "username", null: false
+    t.string "email", null: false
     t.string "encrypted_password", default: "", null: false, comment: "Required if console is true"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
