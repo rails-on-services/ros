0225068993e7d2bc69827f51afbad8efae76e63a
Sergey Toy <sergeytoy@perxtech.com>
2019-10-24 14:07:57 +0800

    wrap Jwt class to Ros module

--- lib/core/lib/ros/jwt.rb
+++ lib/core/lib/ros/jwt.rb
@@ -12,46 +12,47 @@
 #   token.merge!(exp: issued_at + expires_in) if expires_in = Settings.dig(:jwt, :token_expires_in_seconds)
 #   token
 # end
+module Ros
+  class Jwt
+    attr_reader :claims, :token
 
-class Jwt
-  attr_reader :claims, :token
-
-  def initialize(payload)
-    if payload.is_a? Hash # From IAM::User, IAM::Root
-      @claims = payload.merge(default_payload)
-    else # From a bearer token
-      @token = payload.gsub('Bearer ', '')
-      decode
+    def initialize(payload)
+      if payload.is_a? Hash # From IAM::User, IAM::Root
+        @claims = payload.merge(default_payload)
+      else # From a bearer token
+        @token = payload.gsub('Bearer ', '')
+        decode
+      end
     end
+
+    def default_payload
+      issued_at = Time.now.to_i
+      token = (expires_in = Settings.dig(:jwt, :token_expires_in_seconds)) ? { exp: issued_at + expires_in } : {}
+      token.merge(iss: iss, aud: aud, iat: issued_at)
+    end
+
+    def add_claims(claims = {})
+      @claims.merge!(claims.select{ |k, v| k.to_s.in?(valid_claims) })
+      self
+    end
+
+    def valid_claims; @valid_claims ||= (Settings.dig(:jwt, :valid_claims) || []) end
+
+    # TODO: Set audience from the issuer's domain name
+    def aud; Settings.jwt.aud end
+
+    def iss; Settings.jwt.iss end
+
+    def encode
+      @token = JWT.encode(claims, encryption_key, alg)
+    end
+
+    def decode
+      @claims = JWT.decode(token, encryption_key, alg).first
+    end
+
+    def encryption_key; Settings.jwt.encryption_key end
+
+    def alg; Settings.jwt.alg end
   end
-
-  def default_payload
-    issued_at = Time.now.to_i
-    token = (expires_in = Settings.dig(:jwt, :token_expires_in_seconds)) ? { exp: issued_at + expires_in } : {}
-    token.merge(iss: iss, aud: aud, iat: issued_at)
-  end
-
-  def add_claims(claims = {})
-    @claims.merge!(claims.select{ |k, v| k.to_s.in?(valid_claims) })
-    self
-  end
-
-  def valid_claims; @valid_claims ||= (Settings.dig(:jwt, :valid_claims) || []) end
-
-  # TODO: Set audience from the issuer's domain name
-  def aud; Settings.jwt.aud end
-
-  def iss; Settings.jwt.iss end
-
-  def encode
-    @token = JWT.encode(claims, encryption_key, alg)
-  end
-
-  def decode
-    @claims = JWT.decode(token, encryption_key, alg).first
-  end
-
-  def encryption_key; Settings.jwt.encryption_key end
-
-  def alg; Settings.jwt.alg end
 end
