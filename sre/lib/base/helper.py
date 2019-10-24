9ecbcc1f1bd5f7443ba939fc26e8540f755a76b3
Shi Hao <shihaotan91@gmail.com>
2019-10-24 11:12:04 +0800

    remove authorization method

--- sre/lib/base/helper.py
+++ sre/lib/base/helper.py
@@ -3,12 +3,9 @@ import pdb
 import os
 from faker import Faker
 
-def authorization(self):
-  return self.token
-
 def create_iam_user(self):
   payload =  { "data": { "type": "users", "attributes": { "username": Faker().name(), "time_zone": "SGT" } } }
-  self.client.post('iam/users', data=json.dumps(payload), headers={"authorization": authorization(self), 'content-type': 'application/vnd.api+json'})
+  self.client.post('iam/users', data=json.dumps(payload), headers={"authorization": self.token, 'content-type': 'application/vnd.api+json'})
 
 def login_as_iam_user(self):
   payload = { "data": { "attributes": { "account_id": "telco", "username": "Admin", "password": "asdfjkl;" } } }
@@ -16,11 +13,11 @@ def login_as_iam_user(self):
 
 def create_cognito_user(self, identifier):
   payload =  { "data": { "type": "users", "attributes": { "primary_identifier": identifier } } }
-  return self.client.post('cognito/users', data=json.dumps(payload), headers={"authorization": authorization(self), 'content-type': 'application/vnd.api+json'} )
+  return self.client.post('cognito/users', data=json.dumps(payload), headers={"authorization": self.token, 'content-type': 'application/vnd.api+json'} )
 
 def login_cognito_user(self, identifier):
   payload = { "data": { "attributes": { "primary_identifier": identifier } } }
-  return self.client.post('cognito/login', data=json.dumps(payload), headers={"authorization": authorization(self), 'content-type': 'application/vnd.api+json' } )
+  return self.client.post('cognito/login', data=json.dumps(payload), headers={"authorization": self.token, 'content-type': 'application/vnd.api+json' } )
 
 def create_and_login_as_cognito_user(self):
   iam_login_response = login_as_iam_user(self)
