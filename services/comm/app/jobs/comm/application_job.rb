5d2897827952b95fad697cfe0a1e9a6730a73698
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-10-24 19:07:06 +0800

    WIP - using base ros operation class

--- services/comm/app/jobs/comm/application_job.rb
+++ services/comm/app/jobs/comm/application_job.rb
@@ -2,22 +2,5 @@
 
 module Comm
   class ApplicationJob < Ros::ApplicationJob
-    # NOTE: Temporarily living here until we have the parent class
-    # handling the jobs and invoking the operations
-    queue_as :comm_default
-
-    # NOTE: perform, in order to interact with TRB operation, needs to
-    # pass the params as an hash
-    def perform(params)
-      OperationResult.new(*operation_class.call(params))
-    end
-
-    private
-
-    def operation_class
-      operation_class_name = self.class.name
-      operation_class_name.slice!('Job')
-      operation_class_name.constantize
-    end
   end
 end
