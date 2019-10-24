57a894478a249faa8db51ad0785f9e8d2b41f3a3
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-10-24 19:03:07 +0800

    WIP - added todo

--- services/comm/app/jobs/message_job.rb
+++ services/comm/app/jobs/message_job.rb
@@ -4,6 +4,7 @@ class MessageJob < Comm::ApplicationJob
   # MessagesController receives a POST request to create a message (sms) with details of from, to and body
   # After the record is created, a Job is created to send to the destination
   # This means that the correct tenant must be selected by apartment
+  # TODO: Discuss how to perform this job...
   def perform(params)
     message = Message.find_by(params)
     message.provider.send(message.channel, message.to, message.from)
