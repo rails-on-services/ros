cb1d27f6b3be36e73529330e7befad00031bd723
shihaotan91 <shihaotan91@gmail.com>
2019-10-24 11:04:20 +0800

    [WIP] [PW-2107] Cloud event serialization fails for json data type (#108)
    * change data to string if it is hash or array
    
    * add urn to org avsc file
    
    * remove urn in org avsc
    
    * add tenant and branch avsc files for organization
    
    * shift data manipulation to application record concern
    
    * update cloud event data method on application record concern and change organization org avsc file
    
    * add more data types to dictionary
    
    * remove string quotations on default null
    
    * updated avro generator
    
    * update cognito avsc files
    
    * update iam avsc files
    
    * update organization avsc files
    
    * create comm avsc files
    
    * update cognito avsc files
    
    * update iam avsc files
    
    * update organization avsc files
    
    * create avsc files for storage
    
    * refactor cloud_event_data method

--- lib/core/app/models/concerns/ros/application_record_concern.rb
+++ lib/core/app/models/concerns/ros/application_record_concern.rb
@@ -38,12 +38,31 @@ module Ros
       after_commit :stream_cloud_event, if: -> { Settings.event_logging.enabled }
 
       def stream_cloud_event
-        type = "#{Settings.service.name}.#{self.class.name.downcase}"
-        Ros::StreamCloudEventJob.perform_later(type, id, as_json)
+        type = "#{Settings.service.name}.#{self.class.name.underscore}"
+        Ros::StreamCloudEventJob.perform_later(type, id, cloud_event_data)
       end
 
       def as_json(*)
         super.merge('urn' => to_urn)
+      end
+
+      def cloud_event_data
+        attributes_to_modify = %i[jsonb datetime]
+        avro_attributes = attributes.map do |name, value|
+          next [name, value] unless attributes_to_modify.include? column_for_attribute(name).type
+
+          convert_attributes(name, value)
+        end
+
+        avro_attributes.to_h.merge('urn' => to_urn)
+      end
+
+      def convert_attributes(name, value)
+        if column_for_attribute(name).type == :jsonb
+          [name, value.to_s]
+        elsif column_for_attribute(name).type == :datetime
+          [name, (value.to_f * 1000).to_i]
+        end
       end
     end
     # rubocop:enable Metrics/BlockLength
