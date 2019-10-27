ef6c99009880aad0a75953a5484fd4151db83bb4
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-10-27 11:55:42 +0800

    WIP - refactored storage document processor to remove rubocop errors and follow a closer approach to steps

--- lib/core/app/operations/ros/storage_document_process.rb
+++ lib/core/app/operations/ros/storage_document_process.rb
@@ -2,8 +2,6 @@
 
 module Ros
   class StorageDocumentProcess
-    attr_accessor :document
-
     # When the remote service's worker picks the job from its queue it needs the following information:
     # The platform_event_data which consists of:
     #   The transfer_map target, e.g. user, which is the remote service's class that will be invoked
@@ -11,16 +9,33 @@ module Ros
     #   The path to the file on the bucket
     # It then determines the file type and does whatever it is supposed to do for a file of this type
     def call(json)
-      id = JSON.parse(json)['id']
-      @document = Ros::Storage::Document.find(id).first
-      Ros::Infra.resources.storage.app.cp(source_path)
-      if target_class.load_document(local_path, document.column_map, true)
-        document.update(platform_event_state: :processed)
-      end
+      setup(JSON.parse(json)['id'])
+      copy_from_source
+      return unless target_class.load_document(local_path, @document.column_map, true)
+
+      @document.update(platform_event_state: :processed)
     end
 
-    def target_class; document.target.classify.constantize end
-    def local_path; "#{Rails.root}/tmp/fs/#{File.basename(source_path)}" end
-    def source_path; document.blob['key'] end
+    private
+
+    def setup(id)
+      @document = Ros::Storage::Document.find(id).first
+    end
+
+    def copy_from_source
+      Ros::Infra.resources.storage.app.cp(source_path)
+    end
+
+    def target_class
+      @document.target.classify.constantize
+    end
+
+    def local_path
+      "#{Rails.root}/tmp/fs/#{File.basename(source_path)}"
+    end
+
+    def source_path
+      @document.blob['key']
+    end
   end
 end
