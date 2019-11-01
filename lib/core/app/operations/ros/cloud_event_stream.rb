66d05b2ccf5a8c29995f2acbd32bb5c832061b8d
Sergey Toy <sergeytoy@perxtech.com>
2019-11-01 10:48:52 +0800

    updated cloud event stream

--- lib/core/app/operations/ros/cloud_event_stream.rb
+++ lib/core/app/operations/ros/cloud_event_stream.rb
@@ -2,16 +2,23 @@
 
 module Ros
   class CloudEventStream < Ros::ActivityBase
+    # rubocop:disable Style/SignalException
     step :log_event
-    # fail :log_error
+    fail :log_error
+    # rubocop:enable Style/SignalException
 
+    # rubocop:disable Lint/UnreachableCode
     def log_event(_ctx, type:, message_id:, data:, **)
-      Rails.configuration.x.event_logger.log_event(type, message_id, data) # => Faraday::Response
-      true
+      ctx[:res] = Rails.configuration.x.event_logger.log_event(type, message_id, data) # => Faraday::Response
+      ctx[:res].success?
+    rescue Faraday::Error => e
+      ctx[:res] = OpenStruct.new(reason_phrase: e.message)
+      false
     end
+    # rubocop:enable Lint/UnreachableCode
 
-    # def log_error(_ctx, errors:, **)
-    #   errors.add(:logger, "Failed to log event")
-    # end
+    def log_error(_ctx, res:, errors:, **)
+      errors.add(:logger, res.reason_phrase)
+    end
   end
 end
