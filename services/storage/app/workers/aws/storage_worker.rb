1c0f5d991b93963f33b600a6cfc697f82df9ba82
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-11-05 10:55:55 +0800

    [Hotfix] StorageWorker initialized only if the Shoryuken is defined

--- services/storage/app/workers/aws/storage_worker.rb
+++ services/storage/app/workers/aws/storage_worker.rb
@@ -1,19 +1,23 @@
 # frozen_string_literal: true
 
-require_relative '../../../spec/dummy/config/application'
-Rails.application.initialize!
+if defined?(Shoryuken)
+  require_relative '../../../spec/dummy/config/application'
+  Rails.application.initialize!
+end
 
 module Aws
   class StorageWorker
-    include Shoryuken::Worker
+    if defined?(Shoryuken)
+      include Shoryuken::Worker
+      shoryuken_options queue: Ros::Infra.resources.mq.storage_data.name, auto_delete: true
 
-    shoryuken_options queue: Ros::Infra.resources.mq.storage_data.name, auto_delete: true
-    Rails.logger.debug("Configured to receive events from queue: #{Ros::Infra.resources.mq.storage_data.name}")
+      Rails.logger.debug("Configured to receive events from queue: #{Ros::Infra.resources.mq.storage_data.name}")
 
-    # Process a lifecycle event from the S3 bucket
-    def perform(_sqs_msg, payload)
-      message = Ros::Infra::Aws::StorageMessage.new(payload: payload)
-      Document.attach_from_storage_events(message.events)
+      # Process a lifecycle event from the S3 bucket
+      def perform(_sqs_msg, payload)
+        message = Ros::Infra::Aws::StorageMessage.new(payload: payload)
+        Document.attach_from_storage_events(message.events)
+      end
     end
   end
 end
