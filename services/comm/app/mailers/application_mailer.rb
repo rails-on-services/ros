4f161623768be81d55978e9b06976621e32d4937
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-11-05 10:35:22 +0800

    [Hotfix] Fixed comm and iam init

--- services/comm/app/mailers/application_mailer.rb
+++ services/comm/app/mailers/application_mailer.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
-module Ros
-  module Comm
-    class ApplicationMailer < ActionMailer::Base
-      default from: 'from@example.com'
-      layout 'mailer'
-    end
-  end
+class ApplicationMailer < ActionMailer::Base
+  default from: 'from@example.com'
+  layout 'mailer'
 end
