202b0c8b6d6d2ecc058b3984b6b38fe949762fd4
Sergey Toy <sergeytoy@perxtech.com>
2019-11-06 14:47:12 +0800

    Ros::Urn.merge_urns implementation

--- lib/core/lib/ros/urn.rb
+++ lib/core/lib/ros/urn.rb
@@ -13,6 +13,67 @@ module Ros
       @resource = resource
     end
 
+    def self.merge_urns(urns)
+      output = []
+      while true do
+        for a in urns do
+          for b in urns do
+            merged = Ros::Urn.campare_urns(a, b)
+            unless merged.nil?
+              output.reject!{|i| i == a}
+              a = merged
+            end
+            output << a
+            output.uniq!
+            output.reject!{|i| i.nil? }
+          end
+        end
+        break if urns.sort == output.sort
+
+        urns = output
+        output = []
+      end
+      output
+    end
+
+    def self.campare_urns(left, right)
+      urn_breakdown = [
+        { txt: 1 },
+        { partition_name: 2 },
+        { service_name: 3 },
+        { region: 4 },
+        { account_id: 5 },
+        { resource: 6 }
+      ]
+
+      left, right = Ros::Urn.from_urn(Ros::Urn.flatten(left)), Ros::Urn.from_urn(Ros::Urn.flatten(right))
+      camparing_results = []
+      consumer, wildcard_position = nil
+      return nil if left.to_s == right.to_s
+      urn_breakdown.each do |i|
+        key = i.keys.first
+        position = i.values.first
+        left_value, right_value = left.send(key), right.send(key)
+
+        if consumer.nil?
+          if left_value != right_value && [left_value, right_value].include?('*')
+            (consumer, wildcard_position = left, position) if left_value == '*'
+            (consumer, wildcard_position = right, position) if right_value == '*'
+          end
+        end
+        camparing_results << (left_value == right_value || [left_value, right_value].include?('*'))
+      end
+      if camparing_results.all?
+        if consumer.nil?
+          nil
+        else
+          consumer.to_s
+        end
+      else
+        nil
+      end
+    end
+
     def self.flatten(urn)
       splitted = urn.split(':')
       if splitted.last.eql?('*') || splitted.size == 6
@@ -59,5 +120,9 @@ module Ros
     # rubocop:enable Rails/DynamicFindBy
 
     def to_s; to_a.join(':') end
+
+    def to_a
+      [@txt, @partition_name, @service_name, @region, @account_id, @resource]
+    end
   end
 end
