4107df32ad113239afd947463f880a20adc67858
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-11-06 19:11:39 +0800

    WIP - removed old code

--- services/cognito/app/controllers/users_controller.rb
+++ services/cognito/app/controllers/users_controller.rb
@@ -1,20 +1,4 @@
 # frozen_string_literal: true
 
 class UsersController < Cognito::ApplicationController
-  # TODO: Should this be in a separate controller nested under users?
-  def merge
-    res = UsersMerge.call(params: merge_params, id: params[:id], current_user: current_user)
-    if res.success?
-      render json: json_resource(resource_class: TransactionResource, record: res.model)
-    else
-      resource = ApplicationResource.new(res, nil)
-      handle_exceptions JSONAPI::Exceptions::ValidationErrors.new(resource)
-    end
-  end
-
-  private
-
-  def merge_params
-    params.permit_all!
-  end
 end
--- services/cognito/app/operations/chown_request_process.rb
+++ services/cognito/app/operations/chown_request_process.rb
@@ -18,6 +18,7 @@ class ChownRequestProcess < Ros::ActivityBase
 
   def create_chown_results(_ctx, id:, from_ids:, to_id:, **)
     from_ids.each do |from_id|
+      # %w[survey game instant-outcome voucher outcome]
       %w[game].each do |service|
         ChownResult.create(chown_request_id: id, service_name: service,
                            from_id: from_id, to_id: to_id, status: 'pending')
