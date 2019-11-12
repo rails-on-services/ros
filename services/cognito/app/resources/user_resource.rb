8689c3b9f21a91a6d5c6ee1349b855588369f085
Admin User <vagrant@whistler-services.local>
2019-11-12 02:54:13 +0000

    perform ID query if query string has digits only

--- services/cognito/app/resources/user_resource.rb
+++ services/cognito/app/resources/user_resource.rb
@@ -6,12 +6,22 @@ class UserResource < Cognito::ApplicationResource
   filter :primary_identifier
 
   filter :query, apply: lambda { |records, value, _options|
-    filter_fields = %w[primary_identifier first_name last_name email_address]
-                    .map { |field| "#{field} ILIKE '%#{value[0]}%'" }
-                    .join(' OR ')
-
+    filter_fields = value[0].scan(/\D/).empty? ? query_id(value[0]) : query_non_id_atrributes(value[0])
     records.where(filter_fields)
   }
 
   has_many :pools
+
+
+  private 
+
+  def query_non_id_atrributes(value)
+    %w[primary_identifier first_name last_name email_address]
+    .map { |field| "#{field} ILIKE '%#{value}%'" }
+    .join(' OR ')
+  end
+
+  def query_id(value)
+    "id IN (#{value})"
+  end
 end
