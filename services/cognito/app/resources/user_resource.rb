a0b0aea7b84d89b7b422498fc3dbc5d0aaec36b0
prasanthkrajan <prasanth.k.rajan@gmail.com>
2019-11-13 06:09:39 +0000

    Uses match to check presence of alphanumerice, instead of scan

--- services/cognito/app/resources/user_resource.rb
+++ services/cognito/app/resources/user_resource.rb
@@ -10,7 +10,7 @@ class UserResource < Cognito::ApplicationResource
     query_by_non_id_attrs = %w[primary_identifier first_name last_name email_address]
                             .map { |field| "#{field} ILIKE '%#{value[0]}%'" }
                             .join(' OR ')
-    filter_fields = value[0].scan(/\D/).empty? ? query_by_id : query_by_non_id_attrs
+    filter_fields = value[0].match(/\D/).nil? ? query_by_id : query_by_non_id_attrs
     records.where(filter_fields)
   }
 
