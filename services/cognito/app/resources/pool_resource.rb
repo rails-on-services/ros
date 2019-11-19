a8260aa1ade667057c9063d5016d6e640d68ca9b
prasanthkrajan <prasanth.k.rajan@gmail.com>
2019-11-19 09:45:10 +0000

    Add test to be able to handle ID query

--- services/cognito/app/resources/pool_resource.rb
+++ services/cognito/app/resources/pool_resource.rb
@@ -4,7 +4,13 @@ class PoolResource < Cognito::ApplicationResource
   attributes :name, :properties
   has_many :users
 
-  filter :name, apply: lambda { |records, value, _options|
-    records.where('name ILIKE ?', "%#{value[0]}%")
+  filter :query, apply: lambda { |records, value, _options|
+    query_by_id = "pools.id IN (#{value[0]})"
+    query_by_non_id_attrs = %w[name]
+                            .map { |field| "#{field} ILIKE '%#{value[0]}%'" }
+                            .join(' OR ')
+
+    filter_fields = /\D/.match?(value[0]) ? query_by_non_id_attrs : query_by_id
+    records.where(filter_fields)
   }
 end
--- services/cognito/spec/requests/pools_spec.rb
+++ services/cognito/spec/requests/pools_spec.rb
@@ -46,58 +46,105 @@ RSpec.describe 'pools requests', type: :request do
       end
     end
 
-    context 'filter names' do
+    context 'perform query' do
       include_context 'authorized user'
 
       let!(:pool_name) { 'Pool-One'}
       let!(:model) { create(:pool, name: pool_name) }
       let!(:user)  { create(:user) }
       let!(:user_pool) { create(:user_pool, pool: model, user: user)}
+      let!(:random_id) { model.id + 10 }
      
       before do
         get url, headers: request_headers
       end
 
-      context 'with match found' do
-        context 'without users included' do
-          let(:url) { "#{base_url}?filter[name]=#{pool_name}" }
+      context 'based on ID' do
+        context 'with match found' do
+          context 'without users included' do
+            let(:url) { "#{base_url}?filter[query]=#{model.id}" }
 
-          it 'returns successful response' do
-            expect(response).to have_http_status(:ok)
-            expect_json_sizes('data', 1)
-            expect_json('included', nil)
-            expect_json('data.0.attributes.name', pool_name)
+            it 'returns successful response' do
+              expect(response).to have_http_status(:ok)
+              expect_json_sizes('data', 1)
+              expect_json('included', nil)
+              expect_json('data.0.id', model.id.to_s)
+            end
           end
+
+          context 'with users included' do
+            let(:url) { "#{base_url}?include=users&filter[query]=#{model.id}" }
+
+            it 'returns successful response' do
+              expect(response).to have_http_status(:ok)
+              expect_json_sizes('data', 1)
+              expect_json_types('included', :array)
+              expect_json('data.0.id', model.id.to_s)
+            end
+          end 
         end
 
-        context 'with users included' do
-          let(:url) { "#{base_url}?include=users&filter[name]=#{pool_name}" }
+        context 'with no match found' do 
+          context 'without users included' do
+            let(:url) { "#{base_url}?filter[query]=#{random_id}" }
 
-          it 'returns successful response' do
-            expect(response).to have_http_status(:ok)
-            expect_json_sizes('data', 1)
-            expect_json_types('included', :array)
-            expect_json('data.0.attributes.name', pool_name)
+            it 'returns successful response with zero result' do
+              expect_json_sizes('data', 0)
+            end
           end
-        end 
+
+          context 'with users included' do
+            let(:url) { "#{base_url}?include=users&filter[query]=#{random_id}" }
+
+            it 'returns successful response with zero result' do
+              expect_json_sizes('data', 0)
+            end
+          end 
+        end
       end
 
-      context 'with no match found' do 
-        context 'without users included' do
-          let(:url) { "#{base_url}?filter[name]=some_random_name" }
+      context 'based on name' do
+        context 'with match found' do
+          context 'without users included' do
+            let(:url) { "#{base_url}?filter[query]=#{pool_name}" }
 
-          it 'returns successful response with zero result' do
-            expect_json_sizes('data', 0)
+            it 'returns successful response' do
+              expect(response).to have_http_status(:ok)
+              expect_json_sizes('data', 1)
+              expect_json('included', nil)
+              expect_json('data.0.attributes.name', pool_name)
+            end
           end
+
+          context 'with users included' do
+            let(:url) { "#{base_url}?include=users&filter[query]=#{pool_name}" }
+
+            it 'returns successful response' do
+              expect(response).to have_http_status(:ok)
+              expect_json_sizes('data', 1)
+              expect_json_types('included', :array)
+              expect_json('data.0.attributes.name', pool_name)
+            end
+          end 
         end
 
-        context 'with users included' do
-          let(:url) { "#{base_url}?include=users&filter[name]=kiwi" }
+        context 'with no match found' do 
+          context 'without users included' do
+            let(:url) { "#{base_url}?filter[query]=some_random_name" }
 
-          it 'returns successful response with zero result' do
-            expect_json_sizes('data', 0)
+            it 'returns successful response with zero result' do
+              expect_json_sizes('data', 0)
+            end
           end
-        end 
+
+          context 'with users included' do
+            let(:url) { "#{base_url}?include=users&filter[query]=kiwi" }
+
+            it 'returns successful response with zero result' do
+              expect_json_sizes('data', 0)
+            end
+          end 
+        end
       end
     end
   end
