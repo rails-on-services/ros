5c16ad13c641143ffc2f3ce713622caa5e15e4cf
prasanthkrajan <prasanth.k.rajan@gmail.com>
2019-11-21 04:06:14 +0000

    Tests content rendered when valid and invalid keys are passed to template

--- services/comm/spec/models/template_spec.rb
+++ services/comm/spec/models/template_spec.rb
@@ -5,8 +5,30 @@ require 'rails_helper'
 RSpec.describe Template, type: :model do
   include_examples 'application record concern' do
     let(:tenant) { Tenant.first }
-    let!(:subject) { create(factory_name) }
+    let!(:first_name) { 'Jim' }
+    let!(:title) { 'Mr.' }
+    let!(:primary_identifier) { 'random-123-primary-identifier' }
+    let!(:user)	{ create(:user, first_name: first_name, primary_identifier: primary_identifier, title: title) }
+    let!(:campaign) { create(:campaign) }
   end
 
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'render' do
+  	context 'when valid keys are passed' do
+  		let(:template) { create(factory_name, content: 'Hi [userFirstName] [userId] [salutation]') }
+  		let (:valid_content) { "Hi #{first_name} #{primary_identifier} #{title}"}
+
+  		it 'renders valid content' do
+		 		expect(template.render(user, campaign)).to eq valid_content
+		 	end
+  	end
+
+  	context 'when invalid keys are passed' do
+  		let(:template) { create(factory_name, content: 'Hi [userFirstName] [userId] [salutationn]') }
+  		let (:invalid_content) { "Hi #{first_name} #{primary_identifier} salutationn"}
+
+  		it 'renders content with raw invalid key' do
+  			expect(template.render(user, campaign)).to eq invalid_content
+  		end
+  	end
+  end
 end
