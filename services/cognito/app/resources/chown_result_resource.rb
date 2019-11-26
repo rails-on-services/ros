c9c189cd189ea69527ff827c265facb294157b82
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-11-26 10:10:45 +0800

    [Hotfix] added missing filter to chown result

--- services/cognito/app/resources/chown_result_resource.rb
+++ services/cognito/app/resources/chown_result_resource.rb
@@ -3,4 +3,6 @@
 class ChownResultResource < Cognito::ApplicationResource
   attributes :from_id, :to_id, :status, :service_name, :chown_request_id
   has_one :chown_request
+
+  filters :from_id, :to_id, :status
 end
