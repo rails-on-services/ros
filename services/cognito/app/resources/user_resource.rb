9ac995b4ef9f63ada9b44dcb52c38b26ec6d2b0c
Sergey Toy <toydestroyer@users.noreply.github.com>
2019-11-26 13:50:57 +0800

    [PW-1783] Search cognito audience list based on contact number
    [User is unable to search audience list  based on contact number](https://perxtechnologies.atlassian.net/browse/PW-1783)
    
    # Describe the changes made. What does this PR changes that might be critical. If any critical decisions have been made, make sure you explain the rationale for these decisions.
    - SQLi fixed
    - `phone_number` added to filter fields.
    
    # How does the implementation addresses the problem
    
    After merging this, we will be able to search the audience by phone number.

--- services/cognito/app/resources/user_resource.rb
+++ services/cognito/app/resources/user_resource.rb
@@ -6,13 +6,13 @@ class UserResource < Cognito::ApplicationResource
   filter :primary_identifier
 
   filter :query, apply: lambda { |records, value, _options|
-    query_by_id = "users.id IN (#{value[0]})"
-    query_by_non_id_attrs = %w[primary_identifier first_name last_name email_address]
-                            .map { |field| "#{field} ILIKE '%#{value[0]}%'" }
+    query_by_non_id_attrs = %w[primary_identifier first_name last_name phone_number email_address]
+                            .map { |field| "#{field} ILIKE :non_id_query" }
                             .join(' OR ')
 
-    filter_fields = /\D/.match?(value[0]) ? query_by_non_id_attrs : query_by_id
-    records.where(filter_fields)
+    results = records.where(query_by_non_id_attrs, non_id_query: "%#{value[0]}%")
+    results = results.or(records.where(id: value[0])) if /^(\d)+$/.match?(value[0])
+    results
   }
 
   has_many :pools
