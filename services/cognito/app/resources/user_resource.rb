c8a56ccb0fe29d2a6b57cbb739592ce633a1f9ec
Sergey Toy <sergeytoy@perxtech.com>
2019-11-26 11:32:18 +0800

    Fix SQL Injection vulnerability, add phone_number to filter fields

--- services/cognito/app/resources/user_resource.rb
+++ services/cognito/app/resources/user_resource.rb
@@ -6,13 +6,13 @@ class UserResource < Cognito::ApplicationResource
   filter :primary_identifier
 
   filter :query, apply: lambda { |records, value, _options|
-    query_by_id = "users.id IN (#{value[0]})"
-    query_by_non_id_attrs = %w[primary_identifier first_name last_name email_address]
-                            .map { |field| "#{field} ILIKE '%#{value[0]}%'" }
+    query_by_id = 'users.id IN (:id_query)'
+    query_by_non_id_attrs = %w[primary_identifier first_name last_name phone_number email_address]
+                            .map { |field| "#{field} ILIKE :non_id_query" }
                             .join(' OR ')
 
-    filter_fields = /\D/.match?(value[0]) ? query_by_non_id_attrs : query_by_id
-    records.where(filter_fields)
+    filter_fields = "#{query_by_id} OR #{query_by_non_id_attrs}"
+    records.where(filter_fields, id_query: value[0].to_i, non_id_query: "%#{value[0]}%")
   }
 
   has_many :pools
