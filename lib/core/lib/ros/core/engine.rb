497485a8c7a1a0a2e1c085ed3d9eab38dbb8fbcd
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-11-27 17:58:23 +0800

    WIP - requiring sentry dns if env var set

--- lib/core/lib/ros/core/engine.rb
+++ lib/core/lib/ros/core/engine.rb
@@ -182,12 +182,11 @@ module Ros
       # Configure any error reporting services if their credential has been set
       # For now, only sentry.io is supported
       initializer 'ros_core.configure_error_reporting' do |_app|
-        binding.pry
         # export PLATFORM__CREDENTIALS__SENTRY_DSN=url
-        if Settings.dig(:credentials, :sentry_dsn)
+        if ENV['SENTRY_DSN']
           require 'sentry-raven'
           Raven.configure do |config|
-            config.dsn = Settings.credentials.sentry_dsn
+            config.dsn = ENV['SENTRY_DSN']
           end
         end
       end
