be26d46e211cba1c0944cd50ce02b918729e9152
Robert Roach <rjayroach@gmail.com>
2019-12-02 01:39:46 +0800

    switch to public schema to process root basic credentials

--- services/iam/lib/ros/api_token_strategy.rb
+++ services/iam/lib/ros/api_token_strategy.rb
@@ -4,12 +4,12 @@ module Ros
   # NOTE: Overrides methods in ros-core gem
   class ApiTokenStrategy
     def authenticate_basic
-      # TODO: Credentials need to be only in the tenant schema, not in public
-      # NOTE: Is that still true that credentials should only be in tenant schema?
-      schema_name = Ros::AccessKey.decode(access_key_id)[:schema_name]
-      return unless (credential = Apartment::Tenant.switch(schema_name) do
+      credential = if access_key[:owner_type].eql?('Root')
+        Apartment::Tenant.switch('public') { Credential.find_by(access_key_id: access_key_id) }
+      else
         Credential.find_by(access_key_id: access_key_id)
-      end)
+      end
+      return unless credential
 
       credential.authenticate_secret_access_key(secret_access_key).try(:owner)
     end
