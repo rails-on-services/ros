98015d7cf0a3de1ed71602d521c84db0d9b3cdd2
Sergey Toy <sergeytoy@perxtech.com>
2019-12-01 13:10:06 +0800

    Update birth_day (_month) filter, regenerate avro schemas

--- services/cognito/app/resources/user_resource.rb
+++ services/cognito/app/resources/user_resource.rb
@@ -3,7 +3,7 @@
 class UserResource < Cognito::ApplicationResource
   attributes :title, :first_name, :last_name, :phone_number, :email_address,
              :primary_identifier, :properties, :anonymous, :birthday
-  filters :primary_identifier, :birthday
+  filter :primary_identifier
 
   filter :query, apply: lambda { |records, value, _options|
     query_by_non_id_attrs = %w[primary_identifier first_name last_name phone_number email_address]
@@ -13,6 +13,14 @@ class UserResource < Cognito::ApplicationResource
     results = records.where(query_by_non_id_attrs, non_id_query: "%#{value[0]}%")
     results = results.or(records.where(id: value[0])) if /^(\d)+$/.match?(value[0])
     results
+  }
+
+  filter :birth_day, apply: lambda { |records, value, _options|
+    records.where("TO_CHAR(birthday, 'DD-MM') = TO_CHAR(DATE(?), 'DD-MM')", value[0])
+  }
+
+  filter :birth_month, apply: lambda { |records, value, _options|
+    records.where("TO_CHAR(birthday, 'MM') = TO_CHAR(DATE(?), 'MM')", value[0])
   }
 
   has_many :pools
