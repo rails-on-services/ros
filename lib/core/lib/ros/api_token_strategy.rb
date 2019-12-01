55862d0a154f54580b864d0507f9afcfcc4557c3
Robert Roach <rjayroach@gmail.com>
2019-12-02 01:39:46 +0800

    switch to public schema to process root basic credentials

--- lib/core/lib/ros/api_token_strategy.rb
+++ lib/core/lib/ros/api_token_strategy.rb
@@ -2,7 +2,7 @@
 
 module Ros
   class ApiTokenStrategy < Warden::Strategies::Base
-    attr_accessor :auth_string, :auth_type, :token, :access_key_id, :secret_access_key, :urn
+    attr_accessor :auth_string, :auth_type, :token, :access_key_id, :secret_access_key, :access_key
 
     # rubocop:disable Lint/DuplicateMethods
     def auth_string; @auth_string ||= env['HTTP_AUTHORIZATION'] end
@@ -12,6 +12,8 @@ module Ros
     def token; @token ||= auth_string&.split&.last end
 
     def access_key_id; @access_key_id ||= token.split(':').first end
+
+    def access_key; @access_key ||= Ros::AccessKey.decode(access_key_id) end
 
     def secret_access_key; @secret_access_key ||= token.split(':').last end
     # rubocop:enable Lint/DuplicateMethods
@@ -28,7 +30,6 @@ module Ros
 
     # TODO: Credential.authorization must be an instance variable
     def authenticate_basic
-      access_key = Ros::AccessKey.decode(access_key_id)
       return unless access_key[:version].positive?
 
       "Ros::IAM::#{access_key[:owner_type]}".constantize.find(access_key[:owner_id]).first
