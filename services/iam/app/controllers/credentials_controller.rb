d76d6cfc212d5ae471499c7f497c1472fa901ff5
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-12-02 13:49:20 +0800

    WIP - moving logic to custom controller

--- services/iam/app/controllers/credentials_controller.rb
+++ services/iam/app/controllers/credentials_controller.rb
@@ -1,4 +1,31 @@
 # frozen_string_literal: true
 
 class CredentialsController < Iam::ApplicationController
+  before_action :validate_platform_owner, only: [:blackcomb]
+
+  # TODO: Remove this once we support registration of callbacks
+  def blackcomb
+    binding.pry
+
+    tenant = Tenant.find_by_schema_or_alias(blackcomb_params[:account_id])
+    credential = tenant.root.credentials.create
+    if credential.persisted?
+      render json: json_resource(resource_class: CredentialResource, record: credential), status: :created
+    else
+      resource = CredentialResource.new(credential, nil)
+      handle_exception JSONAPI::Exceptions::ValidationErrors.new(resource)
+    end
+  end
+
+  private
+
+  def validate_platform_owner
+    return true if Apartment::Tenant.current == 'public' && context[:user].root?
+
+    render json: { errors: ['Invalid credentials'] }, status: :forbidden
+  end
+
+  def blackcomb_params
+    jsonapi_params.permit(:account_id)
+  end
 end
--- services/iam/app/resources/credential_resource.rb
+++ services/iam/app/resources/credential_resource.rb
@@ -1,29 +1,9 @@
 # frozen_string_literal: true
 
 class CredentialResource < Iam::ApplicationResource
-  attributes :access_key_id, :owner_type, :owner_id, :secret_access_key,
-             :account_id
+  attributes :access_key_id, :owner_type, :owner_id, :secret_access_key
 
   filter :access_key_id
-
-  def account_id=(account_id)
-    return unless account_id
-
-    tenant = Tenant.find_by_schema_or_alias(account_id)
-    @model.owner = tenant.root
-  end
-
-  def self.creatable_fields(context)
-    if Apartment::Tenant.current == 'public' && context[:user].root?
-      [:account_id]
-    else
-      %i[owner_type owner_id]
-    end
-  end
-
-  def fetchable_fields
-    super - %i[account_id]
-  end
 
   def self.descriptions
     {
