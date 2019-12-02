ff55a34a5affa1edd0ba946eb591ad9dc8b1c8ff
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-12-02 16:53:37 +0800

    WIP - moved blackcomb credential to operaiton

--- services/iam/app/controllers/credentials_controller.rb
+++ services/iam/app/controllers/credentials_controller.rb
@@ -1,29 +1,23 @@
 # frozen_string_literal: true
 
 class CredentialsController < Iam::ApplicationController
-  before_action :validate_platform_owner, only: [:blackcomb]
-
   # TODO: Remove this once we support registration of callbacks
   def blackcomb
-    tenant = Tenant.find_by_schema_or_alias(blackcomb_params[:account_id])
-    credential = tenant.root.credentials.create
-    if credential.persisted?
-      render json: json_resource(resource_class: CredentialResource, record: credential), status: :created
+    res = BlackcombUserCreate.call(params: blackcomb_params)
+    if res.success?
+      @current_jwt = Ros::Jwt.new(res.model.jwt_payload)
+      render json: json_resource(resource_class: UserResource, record: res.model), status: :created
     else
-      resource = CredentialResource.new(credential, nil)
-      handle_exception JSONAPI::Exceptions::ValidationErrors.new(resource)
+      resource = ApplicationResource.new(res, nil)
+      handle_exceptions JSONAPI::Exceptions::ValidationErrors.new(resource)
     end
   end
 
   private
 
-  def validate_platform_owner
-    return true if Apartment::Tenant.current == 'public' && context[:user].root?
-
-    render json: { errors: ['Invalid credentials'] }, status: :forbidden
-  end
-
   def blackcomb_params
-    jsonapi_params.permit(:account_id)
+    permitted_params = jsonapi_params.permit(:account_id)
+
+    HashWithIndifferentAccess.new({ current_user: context[:user] }.merge(permitted_params))
   end
 end
