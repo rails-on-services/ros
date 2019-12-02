a0e11fb89c190897c5e340bfdbf1edb3aa457d15
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-12-02 11:46:12 +0800

    WIP - cleanup

--- lib/core/app/models/concerns/ros/urn_concern.rb
+++ lib/core/app/models/concerns/ros/urn_concern.rb
@@ -6,21 +6,31 @@ module Ros
 
     class_methods do
       # urn:partition:service:region:account_id:resource_type
-      def to_urn; "#{urn_base}:#{account_id}:#{name.underscore}" end
+      def to_urn
+        "#{urn_base}:#{account_id}:#{name.underscore}"
+      end
 
       # Universal Resource Name (URNs) and Service Namespaces
       # urn:partition:service:region
-      def urn_base; "urn:#{Settings.partition_name}:#{Settings.service.name}:#{Settings.region}" end
+      def urn_base
+        "urn:#{Settings.partition_name}:#{Settings.service.name}:#{Settings.region}"
+      end
 
-      def find_by_urn(value); find_by(urn_id => value) end
+      def find_by_urn(value)
+        find_by(urn_id => value)
+      end
 
       # NOTE: Override in model to provide a custom id
-      def urn_id; :id end
+      def urn_id
+        :id
+      end
     end
 
     included do
       # urn:partition:service:region:account_id:resource_type/id
-      def to_urn; "#{self.class.to_urn}/#{send(self.class.urn_id)}" end
+      def to_urn
+        "#{self.class.to_urn}/#{send(self.class.urn_id)}"
+      end
 
       def urn_match?(urn_to_compare)
         params = %i[txt partition_name service_name region account_id resource]
