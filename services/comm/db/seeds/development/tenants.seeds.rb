56e7d2e217f8856b2fa494e008f37fc4a80bd21c
prasanthkrajan <prasanth.k.rajan@gmail.com>
2019-12-04 03:15:21 +0000

    revert platform enabled twilio changes to comm seed

--- services/comm/db/seeds/development/tenants.seeds.rb
+++ services/comm/db/seeds/development/tenants.seeds.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-Tenant.create(schema_name: 'public', platform_twilio_enabled: true)
+Tenant.create(schema_name: 'public')
 
 1.upto(6) do |id|
-  Tenant.create!(schema_name: Tenant.account_id_to_schema(id.to_s * 9))
+  tenant = Tenant.create!(schema_name: Tenant.account_id_to_schema(id.to_s * 9))
+  tenant.update(platform_twilio_enabled: true) if id == 1
 end
