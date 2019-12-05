f389f87d7e8a9f145202460ea74f91bd025e4672
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-12-05 14:22:41 +0800

    WIP - fixed rb mistake

--- services/organization/db/seeds/development/tenants.seeds.rb
+++ services/organization/db/seeds/development/tenants.seeds.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 t = Tenant.create(schema_name: 'public')
-puts t.errors if t.errors.length > 0
+puts t.errors unless t.errors.empty?
 
 1.upto(6) do |id|
   schema_name = Tenant.account_id_to_schema(id.to_s * 9)
