8e39d8585333f31937b4488ef6cdc7f297fea34c
prasanthkrajan <prasanth.k.rajan@gmail.com>
2019-12-05 06:52:51 +0000

    spec update

--- services/comm/spec/requests/messages_spec.rb
+++ services/comm/spec/requests/messages_spec.rb
@@ -5,9 +5,28 @@ require 'rails_helper'
 RSpec.describe 'Messages', type: :request do
   include_context 'jsonapi requests'
 
-  let(:tenant) { Tenant.first }
-  let(:mock) { true }
-  let(:url) { u('/messages') }
+  let(:tenant)   { Tenant.first }
+  let(:mock)     { true }
+  let(:url)      { u('/messages') }
+  let(:provider) { create(:provider_aws) }
+  let(:post_data) do
+    {
+      data: {
+        attributes: {
+          body: 'HELLO DEAR HELLO',
+          to: '+6587173612',
+          from: 'PerxTest',
+          provider_id: provider.id,
+          channel: 'sms'
+        }
+      }
+    }.to_json
+  end
+
+  before do
+    mock_authentication if mock
+    post url, params: post_data, headers: request_headers
+  end
 
   describe 'POST create' do
     context 'Unauthenticated user' do
@@ -16,21 +35,26 @@ RSpec.describe 'Messages', type: :request do
     end
 
     context 'Authenticated user' do
-      include_context 'authorized user'
+      include_context 'cognito user'
 
       context 'cognito user attempts to send message' do
         it 'should not allow message to be sent' do
+          expect(response).not_to be_successful
         end
       end
 
-      context 'non cognito user attempts to send message' do
+      context 'authorized user attempts to send message' do
+        include_context 'authorized user'
+
         context 'when the payload is valid' do
           it 'should send message successfully' do
+            expect(response).to be_successful
           end
         end
 
         context 'when the payload is invalid' do
           it 'should fail message sending' do
+            expect(response).not_to be_successful
           end
         end
       end
