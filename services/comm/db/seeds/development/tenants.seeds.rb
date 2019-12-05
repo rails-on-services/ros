3dd516aa0df35cbe555a6b789eed614ff071571f
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-12-05 13:22:58 +0800

    WIP -

--- services/comm/db/seeds/development/tenants.seeds.rb
+++ services/comm/db/seeds/development/tenants.seeds.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
-Tenant.create(schema_name: 'public')
+t = Tenant.create(schema_name: 'public')
+puts t.errors if t.errors.length > 0
 
 1.upto(6) do |id|
-  tenant = Tenant.create!(schema_name: Tenant.account_id_to_schema(id.to_s * 9))
+  schema_name = Tenant.account_id_to_schema(id.to_s * 9)
+  puts "CREATING #{schema_name}"
+  tenant = Tenant.create!(schema_name: schema_name)
   tenant.update(platform_twilio_enabled: true) if id == 1
 end
--- services/organization/db/seeds/development/tenants.seeds.rb
+++ services/organization/db/seeds/development/tenants.seeds.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
-Tenant.create(schema_name: 'public')
+t = Tenant.create(schema_name: 'public')
+puts t.errors if t.errors.length > 0
 
 1.upto(6) do |id|
-  Tenant.create!(schema_name: Tenant.account_id_to_schema(id.to_s * 9))
+  schema_name = Tenant.account_id_to_schema(id.to_s * 9)
+  puts "CREATING #{schema_name}"
+  Tenant.create!(schema_name: schema_name)
 end
