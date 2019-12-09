2a87469f4f75b8dc2b4934a19bf44eada95dff2b
prasanthkrajan <prasanth.k.rajan@gmail.com>
2019-12-09 02:00:33 +0000

    test invalid payload fails message sending

--- services/comm/spec/requests/messages_spec.rb
+++ services/comm/spec/requests/messages_spec.rb
@@ -35,9 +35,9 @@ RSpec.describe 'Messages', type: :request do
     end
 
     context 'Authenticated user' do
-      include_context 'cognito user'
-
       context 'cognito user attempts to send message' do
+        include_context 'cognito user'
+
         it 'should not allow message to be sent' do
           expect(response).not_to be_successful
         end
@@ -53,6 +53,20 @@ RSpec.describe 'Messages', type: :request do
         end
 
         context 'when the payload is invalid' do
+          let(:post_data) do
+            {
+              data: {
+                attributes: {
+                  body: 'HELLO DEAR HELLO',
+                  to: '+6587173612',
+                  from: 'PerxTest',
+                  provider_id: nil,
+                  channel: 'sms'
+                }
+              }
+            }.to_json
+          end
+
           it 'should fail message sending' do
             expect(response).not_to be_successful
           end
