86e9c587c1767c40fcf2bd6451c6971e7cf7619d
Sergey Toy <sergeytoy@perxtech.com>
2019-12-09 17:47:11 +0800

    WIP

--- services/cognito/app/operations/segmented_pool_create.rb
+++ services/cognito/app/operations/segmented_pool_create.rb
@@ -3,12 +3,29 @@
 class SegmentedPoolCreate < Ros::ActivityBase
   step :create_pool
   failed :pool_not_created, Output(:success) => End(:failure)
+  step :fetch_users
+  failed :users_not_fetched, Output(:success) => End(:failure)
+  step :add_users_to_pool
 
   def create_pool(ctx, **)
-    ctx[:pool] = Pool.create(name: "temporary_pool_#{SecureRandom.hex}")
+    ctx[:model] = Pool.create(name: "temporary_pool_#{SecureRandom.hex}")
   end
 
   def pool_not_created(_ctx, errors:, **)
     errors.add(:pool, "Can't create temporary pool")
+  end
+
+  def fetch_users(ctx, **)
+    # users
+    ctx[:users] = []
+    true
+  end
+
+  def users_not_fetched(_ctx, errors:, **)
+    errors.add(:users, "Can't fetch users for pool")
+  end
+
+  def add_users_to_pool(_ctx, model:, users:, **)
+    model.users << users
   end
 end
