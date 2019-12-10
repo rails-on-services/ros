bb495c1663370e7c64d265df325e2394cb760afc
Sergey Toy <sergeytoy@perxtech.com>
2019-12-10 12:55:15 +0800

    request specs

--- services/cognito/spec/requests/pools_spec.rb
+++ services/cognito/spec/requests/pools_spec.rb
@@ -151,4 +151,61 @@ RSpec.describe 'pools requests', type: :request do
       end
     end
   end
+
+  describe 'POST create' do
+    context 'Unauthenticated user' do
+      include_context 'unauthorized user'
+      include_examples 'unauthenticated post'
+    end
+
+    context 'Authenticated user' do
+      include_context 'authorized user'
+      let(:model_data) { build(:pool) }
+
+      context 'regular pool creation' do
+        before do
+          mock_authentication if mock
+          post url, headers: request_headers, params: post_data
+        end
+
+        context 'correct params' do
+          let(:post_data) { jsonapi_data(model_data) }
+
+          it 'returns a successful response with proper serialized response' do
+            expect(response).to be_created
+          end
+        end
+
+        context 'incorrect params' do
+          let(:post_data) { jsonapi_data(model_data, extra_attributes: { invalid: :param }) }
+
+          it 'returns a failure response and' do
+            expect(errors.size).to be_positive
+            expect(response).to be_bad_request
+            expect(error_response.title).to eq('Param not allowed')
+          end
+        end
+      end
+
+      context 'segmented pool creation' do
+        let(:base_pool) { create(:pool) }
+        let(:birthday_user) { create(:user, birthday: Time.zone.today) }
+
+        before do
+          mock_authentication if mock
+          base_pool.users << birthday_user
+          post url, headers: request_headers, params: post_data
+        end
+
+        context 'correct params' do
+          let(:url) { "#{base_url}/?base_pool_id=#{base_pool.id}&segments[birthday]=this_day" }
+          let(:post_data) { jsonapi_data(model_data) }
+
+          it 'returns a successful response with proper serialized response' do
+            expect(response).to be_created
+          end
+        end
+      end
+    end
+  end
 end
