1b0e3ef48b6e3ac2de98439913b66a9b38c37489
prasanthkrajan <prasanth.k.rajan@gmail.com>
2019-12-12 07:00:56 +0000

    add operation for msg send

---
+++ services/comm/app/operations/message_send.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class MessageSend < Ros::ActivityBase
+  step :retrieve_message
+  step :send_message
+  step :update_message_provider
+
+  def retrieve_message(ctx, id:, **)
+    ctx[:message] = Message.find_by(id)
+  end
+
+  def send_message(ctx, **)
+    ctx[:msg_id] = ctx[:message].provider.send(ctx[:message].channel, ctx[:message].to, ctx[:message].from)
+  end
+
+  def update_message_provider(ctx, **)
+    ctx[:message].provider_msg_id = ctx[:msg_id]
+    ctx[:message].save
+  end
+end
