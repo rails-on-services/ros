bfe51a78f54a80c25bd90964e83c270616b212b3
prasanthkrajan <prasanth.k.rajan@gmail.com>
2019-12-18 03:16:40 +0000

    add class methods to follow jobs pattern

--- services/storage/app/operations/iam_public_key_process.rb
+++ services/storage/app/operations/iam_public_key_process.rb
@@ -3,33 +3,35 @@
 class IamPublicKeyProcess
   METADATA_HASH = { mode: '33188', gid: '100' }.freeze
 
-  def call(_json)
-    Ros::Infra.resources.storage.app.cp(source_path, target_path, METADATA_HASH.merge(uid: tenant.account_id))
+  class << self
+    def call(_json)
+      Ros::Infra.resources.storage.app.cp(source_path, target_path, METADATA_HASH.merge(uid: tenant.account_id))
+    end
+
+    def source_path; "fs:#{key_file.path.gsub("#{fs_path}/", '')}" end
+
+    def target_path; "#{SftpFile.object_root}/config/sshx/authorized-keys/#{tenant.account_id}" end
+
+    def tenant
+      Tenant.find_by(schema_name: Apartment::Tenant.current)
+    end
+
+    def key_file
+      @key_file ||= tempfile
+    end
+
+    def tempfile
+      file = Tempfile.new('public_key', fs_path)
+      file.write(content)
+      file.close
+      file
+    end
+
+    # TODO: Handle pagination
+    def content
+      @content ||= Ros::IAM::PublicKey.select(:content).all.map(&:content).join("\n")
+    end
+
+    def fs_path; Rails.root.join('tmp', 'fs') end
   end
-
-  def source_path; "fs:#{key_file.path.gsub("#{fs_path}/", '')}" end
-
-  def target_path; "#{SftpFile.object_root}/config/sshx/authorized-keys/#{tenant.account_id}" end
-
-  def tenant
-    Tenant.find_by(schema_name: Apartment::Tenant.current)
-  end
-
-  def key_file
-    @key_file ||= tempfile
-  end
-
-  def tempfile
-    file = Tempfile.new('public_key', fs_path)
-    file.write(content)
-    file.close
-    file
-  end
-
-  # TODO: Handle pagination
-  def content
-    @content ||= Ros::IAM::PublicKey.select(:content).all.map(&:content).join("\n")
-  end
-
-  def fs_path; Rails.root.join('tmp', 'fs') end
 end
