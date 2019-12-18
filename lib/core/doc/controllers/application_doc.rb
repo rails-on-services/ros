53f41f10ccbb8ad50ef7a2a8a0d9872739d33070
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-12-18 13:00:26 +0800

    WIP -

--- lib/core/doc/controllers/application_doc.rb
+++ lib/core/doc/controllers/application_doc.rb
@@ -3,6 +3,17 @@
 # NOTE: Postman does not like type 'application/vnd.api+json'
 # resp 200, 'success', 'application/vnd.api+json', data: { data: [{id: 1}] }
 
+module JSONAPI
+  module Authorization
+    class Configuration
+      def user_context(_context)
+        PolicyUser.new(nil, nil, {})
+        # HashWithIndifferentAccess.new(cognito_user_id: nil, user: nil)
+      end
+    end
+  end
+end
+
 module AutoGenDoc
   def self.included(base)
     base.extend ClassMethods
@@ -27,6 +38,7 @@ module AutoGenDoc
           (subclass.resource_class.filters.keys - %i[id]).each do |field|
             query "filter[#{field}]", String, desc: subclass.resource_class.descriptions[field] || nil
           end
+
           response 200, :success, :json,
                    data: JSONAPI::ResourceSerializer.new(subclass.resource_class)
                                                     .serialize_to_hash(subclass.resources).to_json
@@ -114,12 +126,12 @@ class ApplicationDoc
   class << self
     # TODO: Provide various contexts rather than default to nil; maybe
     def resource
-      model = model_class.first || create(model_name.underscore.to_sym)
+      model = model_class.first || FactoryBot.create(model_name.underscore.to_sym)
       resource_class.new(model, nil)
     end
 
     def resources
-      2.times { create(model_name.underscore.to_sym) } if model_class.count.zero?
+      2.times { FactoryBot.create(model_name.underscore.to_sym) } if model_class.count.zero?
       model_class.all.limit(2).map { |record| resource_class.new(record, nil) }
     end
 
--- services/cognito/doc/resources/chown_request_resource_doc.rb
+++ services/cognito/doc/resources/chown_request_resource_doc.rb
@@ -3,8 +3,5 @@
 class ChownRequestResourceDoc < ApplicationDoc
   route_base 'chown_requests'
 
-  api :index, 'All Chown requests'
-  api :show, 'Single Chown request'
   api :create, 'Create Chown request'
-  api :update, 'Update Chown request'
 end
