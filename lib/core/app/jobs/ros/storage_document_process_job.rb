9f81fd2620abf382af90289592925f0dfd432786
prasanthkrajan <prasanth.k.rajan@gmail.com>
2019-12-18 01:58:14 +0000

    add class methods to storage document process to follow jobs pattern

--- lib/core/app/jobs/ros/storage_document_process_job.rb
+++ lib/core/app/jobs/ros/storage_document_process_job.rb
@@ -2,13 +2,5 @@
 
 module Ros
   class StorageDocumentProcessJob < Ros::ApplicationJob
-    def perform(*params)
-      operation_class(*params).new.call(*params)
-    end
-
-    def operation_class(json)
-      operation = JSON.parse(json)['operation']
-      operation.constantize
-    end
   end
 end
