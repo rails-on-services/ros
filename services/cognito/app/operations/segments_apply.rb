02820c0294de7cf71cb56b6466227e8c84de1954
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-12-19 12:49:35 +0800

    WIP - refactored segment apply

--- services/cognito/app/operations/segments_apply.rb
+++ services/cognito/app/operations/segments_apply.rb
@@ -2,10 +2,11 @@
 
 class SegmentsApply < Ros::ActivityBase
   step :init
-  step :apply_birthday
-  step :apply_except_ids
-  step :apply_gender
-  step :apply_age
+  step :apply_segments
+  # step :apply_birthday
+  # step :apply_except_ids
+  # step :apply_gender
+  # step :apply_age
 
   private
 
@@ -13,10 +14,33 @@ class SegmentsApply < Ros::ActivityBase
     ctx[:model] = users
   end
 
+  def apply_segments(ctx, model:, segments:, errors:, **)
+    segments.keys.each do |segment_key, segment_value|
+      begin
+        puts ">>>>>>>>>>>>>>>>>>>> #{segment_key.classify}"
+        segment_class = "Segments::#{segment_key.classify}".constantize
+        applied_segment = segment_class.call(users: model, segment: segment_value)
+        if applied_segment.failure?
+          errors.add(:segment, applied_segment.errors)
+          return false
+        end
+
+        model = applied_segment.model
+      rescue Exception => e
+        errors.add(:segment, e.inspect)
+        return false
+      end
+    end
+    ctx[:model] = model
+  end
+
   def apply_birthday(ctx, model:, segments:, **)
     return true unless segments.key?(:birthday)
 
-    ctx[:model] = Segments::Birthday.call(users: model, segment: segments[:birthday]).model
+    applied_segment = Segments::Birthday.call(users: model, segment: segments[:birthday])
+    return false if applied_segment.failure?
+
+    ctx[:model] = applied_segment.model
   end
 
   def apply_except_ids(ctx, model:, segments:, **)
