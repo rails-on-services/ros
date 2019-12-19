c7bdbaa863d987b36dbf4799957571d9fc0b4767
Sergey Toy <sergeytoy@perxtech.com>
2019-12-19 13:42:31 +0800

    WIP

---
+++ services/iam/app/operations/user_create.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+class UserCreate < Ros::ActivityBase
+  step :init
+  step :initialize_user
+  step :skip_confirmation_notification
+  step :generate_reset_passowrd_token
+  step :save_user
+  failed :user_not_created
+  step :create_relationships
+  step :send_welcome_email
+
+  private
+
+  def init(ctx, params:, **)
+    ctx[:relationships] = params.delete(:relationships)
+  end
+
+  def initialize_user(ctx, params:, **)
+    ctx[:model] = User.new(params)
+  end
+
+  def skip_confirmation_notification(_ctx, model:, **)
+    model.skip_confirmation_notification!
+  end
+
+  def generate_reset_passowrd_token(ctx, model:, **)
+    # NOTE: Alternative way is
+    # ctx[:reset_password_token] = model.send(:set_reset_password_token)
+    # But it saves model
+    ctx[:reset_password_token], enc = Devise.token_generator.generate(model.class, :reset_password_token)
+
+    model.reset_password_token   = enc
+    model.reset_password_sent_at = Time.now.utc
+  end
+
+  def save_user(_ctx, model:, **)
+    model.save
+  end
+
+  def user_not_created(_ctx, model:, errors:, **)
+    errors.add(:user, model.errors.full_messages)
+  end
+
+  def create_relationships(_ctx, model:, relationships:, **)
+    model.groups << Group.where(id: relationships[:groups][:data].pluck(:id)).all
+  end
+
+  def send_welcome_email(_ctx, model:, reset_password_token:, **)
+    AccountMailer.team_welcome(model, reset_password_token).deliver_later
+  end
+end
