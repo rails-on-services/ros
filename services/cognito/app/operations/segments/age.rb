fda932973a4f1c49e92137789c901153eee6fa8d
Sergey Toy <toydestroyer@users.noreply.github.com>
2019-12-19 18:50:29 +0800

    [PW-2895] Age and gender segments
    # Refer to the issue id if any. Include the link to the issue. E.g:
    [We need 2 more filter, age and gender](https://perxtechnologies.atlassian.net/browse/PW-2895)
    
    # Describe the changes made. What does this PR changes that might be critical. If any critical decisions have been made, make sure you explain the rationale for these decisions.
    - Age segments
    - Gender segments
    
    # How does the implementation addresses the problem
    
    After merging this, we will be able to segment audience by gender and age.

---
+++ services/cognito/app/operations/segments/age.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+module Segments
+  class Age < Ros::ActivityBase
+    step :init
+    step :verify_segment
+    failed :incorrect_segment, Output(:success) => End(:failure)
+    step :translate_segment
+    step :apply_segment
+    failed :segment_not_applied, Output(:success) => End(:failure)
+
+    private
+
+    def init(ctx, segment:, **)
+      ctx[:segment] = Array.wrap(segment)
+      ctx[:translated_segments] = []
+    end
+
+    def verify_segment(_ctx, segment:, **)
+      segment.all? { |s| segment_element_valid?(s) }
+    end
+
+    def incorrect_segment(_ctx, errors:, segment:, **)
+      errors.add(:segment, "Incorrect age segment value: #{segment}")
+    end
+
+    def translate_segment(_ctx, segment:, translated_segments:, **)
+      segment.each do |element|
+        translated_segments << case element
+                               when Hash
+                                 el = element.with_indifferent_access
+                                 date_range(el['from'], el['to'])
+                               when String, Integer
+                                 date_range(element, element)
+                               end
+      end
+    end
+
+    def apply_segment(ctx, users:, translated_segments:, **)
+      ctx[:model] = users.where(birthday: translated_segments)
+    end
+
+    def segment_not_applied(_ctx, errors:, segment:, **)
+      errors.add(:model, "Can't apply age segment: #{segment}")
+    end
+
+    def segment_element_valid?(element)
+      case element
+      when Hash
+        element.with_indifferent_access.keys.sort == %w[from to] && element.values.all? { |el| el.to_s.match(/^\d+$/) }
+      when String, Integer
+        element.to_s.match(/^\d+$/)
+      else
+        false
+      end
+    end
+
+    def date_range(from, to, base_date = Time.zone.today)
+      Range.new(
+        base_date - to.to_i.years - 1.year + 1.day,
+        base_date - from.to_i.years
+      )
+    end
+  end
+end
--- services/cognito/app/operations/segments_apply.rb
+++ services/cognito/app/operations/segments_apply.rb
@@ -2,9 +2,7 @@
 
 class SegmentsApply < Ros::ActivityBase
   step :init
-  step :apply_birthday
-  step :apply_except_ids
-  # step :apply_gender
+  step :apply_segments
 
   private
 
@@ -12,21 +10,26 @@ class SegmentsApply < Ros::ActivityBase
     ctx[:model] = users
   end
 
-  def apply_birthday(ctx, model:, segments:, **)
-    return true unless segments.key?(:birthday)
+  def apply_segments(ctx, model:, segments:, errors:, **)
+    segments.each do |segment_key, segment_value|
+      res = apply_segment(segment_key, segment_value, model, errors)
+      return false if res.nil?
 
-    ctx[:model] = Segments::Birthday.call(users: model, segment: segments[:birthday]).model
+      if res.failure?
+        ctx[:errors] = res.errors
+        return false
+      end
+
+      model = res.model
+    end
+    ctx[:model] = model
   end
 
-  def apply_except_ids(ctx, model:, segments:, **)
-    return true unless segments.key?(:except_ids)
-
-    ctx[:model] = Segments::ExceptIds.call(users: model, segment: segments[:except_ids]).model
+  def apply_segment(segment_key, segment_value, users, errors)
+    segment_class = "Segments::#{segment_key.classify}".constantize
+    segment_class.call(users: users, segment: segment_value)
+  rescue NameError => _e
+    errors.add(:segment, "Can't find segmentation class")
+    nil
   end
-
-  # def apply_gender(ctx, model:, segments:, **)
-  #   return true unless segments.key?('gender')
-
-  #   ctx[:model] = Segments::Gender.call(users: model, segment: segments['gender']).model
-  # end
 end
