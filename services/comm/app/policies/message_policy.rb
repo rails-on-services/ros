7df910b80fdcdf7fa0e3c6689cf2e1ec4c321a06
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-12-26 17:53:03 +0800

    WIP - simplified message policy

--- services/comm/app/policies/message_policy.rb
+++ services/comm/app/policies/message_policy.rb
@@ -2,9 +2,8 @@
 
 class MessagePolicy < Comm::ApplicationPolicy
   def create?
-    can_create = super
-    return can_create unless can_create
+    return false if user.cognito_user_id
 
-    user.cognito_user_id.nil?
+    super
   end
 end
