7899e8be990aea7d2e0f9daeeac3778e7736df86
Shi Hao <shihaotan91@gmail.com>
2019-12-26 16:59:22 +0800

    add iam header to response

--- sre/lib/base/helper.py
+++ sre/lib/base/helper.py
@@ -5,26 +5,34 @@ from faker import Faker
 
 def create_iam_user(self):
   payload =  { "data": { "type": "users", "attributes": { "username": Faker().name(), "time_zone": "SGT" } } }
-  self.client.post('iam/users', data=json.dumps(payload), headers={"authorization": self.token, 'content-type': 'application/vnd.api+json'})
+  self.client.post('iam/users', data=json.dumps(payload), headers=self.iam_header)
 
 def login_as_iam_user(self):
-  payload = { "data": { "attributes": { "account_id": "telco", "username": "Admin", "password": "asdfjkl;" } } }
+  payload = { "data": { "attributes": { "account_id": "generic", "username": "Admin", "password": "asdfjkl;" } } }
   return self.client.post('iam/users/sign_in', data=json.dumps(payload), headers={'content-type': 'application/json'})
 
+def create_cognito_pool(self):
+  pool_name = Faker().pystr()
+  payload = { "data": { "type": "pools", "attributes": { "name": pool_name } } }
+  return self.client.post('cognito/pools', data=json.dumps(payload), headers=self.iam_header )
+
 def create_cognito_user(self, identifier):
-  payload =  { "data": { "type": "users", "attributes": { "primary_identifier": identifier } } }
-  return self.client.post('cognito/users', data=json.dumps(payload), headers={"authorization": self.token, 'content-type': 'application/vnd.api+json'} )
+  payload =  { "data": { "type": "users", "attributes": { "primary_identifier": identifier }, "relationships": { "pools": { "data": [{ "type": "pools", "id": self.pool_id }] } } } }
+  return self.client.post('cognito/users', data=json.dumps(payload), headers=self.iam_header )
 
 def login_cognito_user(self, identifier):
   payload = { "data": { "attributes": { "primary_identifier": identifier } } }
-  return self.client.post('cognito/login', data=json.dumps(payload), headers={"authorization": self.token, 'content-type': 'application/vnd.api+json' } )
+  return self.client.post('cognito/login', data=json.dumps(payload), headers=self.iam_header )
 
 def create_and_login_as_cognito_user(self):
   iam_login_response = login_as_iam_user(self)
-  self.token = iam_login_response.headers['Authorization']
+  self.iam_header = { "authorization": iam_login_response.headers['Authorization'], 'content-type': 'application/vnd.api+json' }
+
+  pool_response = create_cognito_pool(self)
+  self.pool_id = json.loads(pool_response.content)['data']['id']
 
   identifier = Faker().pystr()
   create_cognito_user(self, identifier)
 
   login_response = login_cognito_user(self, identifier)
-  self.header = { "authorization": login_response.headers['Authorization'], 'content-type': 'application/vnd.api+json' }
+  self.cognito_header = { "authorization": login_response.headers['Authorization'], 'content-type': 'application/vnd.api+json' }
--- sre/lib/organization/org.py
+++ sre/lib/organization/org.py
@@ -1,12 +1,12 @@
 import json
 
 def get_all_organization_orgs(self):
-  self.client.get('organization/orgs', headers=self.header)
+  self.client.get('organization/orgs', headers=self.cognito_header)
 
 def get_organization_org(self, id):
   path = ('organization/orgs/%s' %(id))
-  self.client.get(path, headers=self.header)
+  self.client.get(path, headers=self.cognito_header)
 
 def create_organization_orgs(self):
   payload = { 'data': { 'type': 'orgs', 'attributes': { 'name': "MyString", 'description': "MyString", 'properties': {} } } }
-  return self.client.post('organization/orgs', data=json.dumps(payload), headers=self.header)
+  return self.client.post('organization/orgs', data=json.dumps(payload), headers=self.iam_header)
