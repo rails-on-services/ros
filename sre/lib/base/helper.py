941fdc236a8db8f5574ba692d361a1e9f3b5b3b0
Shi Hao <shihaotan91@gmail.com>
2019-12-28 21:26:07 +0800

    make header compulsory field when calling endpoint

--- sre/lib/base/helper.py
+++ sre/lib/base/helper.py
@@ -13,35 +13,35 @@ def login_as_iam_user(self):
   self.iam_header = { "authorization": iam_login_response.headers['Authorization'], 'content-type': 'application/vnd.api+json' }
   return iam_login_response
 
-def create_cognito_pool(self):
+def create_cognito_pool(self, header):
   pool_name = Faker().pystr()
   payload = { "data": { "type": "pools", "attributes": { "name": pool_name } } }
-  pool_response = self.client.post('cognito/pools', data=json.dumps(payload), headers=self.response['iam_header'] )
+  pool_response = self.client.post('cognito/pools', data=json.dumps(payload), headers=header )
   return pool_response
 
-def create_anonymous_user(self, identifier):
+def create_anonymous_user(self, identifier, header):
   payload = { "data": { "type": "users", "attributes": { "primary_identifier": identifier, 'anonymous': True } } }
-  return self.client.post('cognito/users', data=json.dumps(payload), headers=self.response['iam_header'] )
+  return self.client.post('cognito/users', data=json.dumps(payload), headers=header )
 
-def login_anonymous_user(self, identifier):
+def login_anonymous_user(self, identifier, header):
   payload = { "data": { "type": "login", "attributes": { "primary_identifier": identifier } } }
-  login_response = self.client.post('cognito/login', data=json.dumps(payload), headers=self.response['iam_header'] )
+  login_response = self.client.post('cognito/login', data=json.dumps(payload), headers=header )
   self.anonymous_header = { "authorization": login_response.headers['Authorization'], 'content-type': 'application/vnd.api+json' }
 
-def create_cognito_user(self, identifier, pool_id):
+def create_cognito_user(self, identifier, pool_id, header):
   payload =  { "data": { "type": "users", "attributes": { "primary_identifier": identifier }, "relationships": { "pools": { "data": [{ "type": "pools", "id": pool_id }] } } } }
-  return self.client.post('cognito/users', data=json.dumps(payload), headers=self.response['iam_header'] )
+  return self.client.post('cognito/users', data=json.dumps(payload), headers=header )
 
-def login_cognito_user(self, identifier):
+def login_cognito_user(self, identifier, header):
   payload = { "data": { "attributes": { "primary_identifier": identifier } } }
-  login_response = self.client.post('cognito/login', data=json.dumps(payload), headers=self.response['iam_header'] )
+  login_response = self.client.post('cognito/login', data=json.dumps(payload), headers=header )
   self.cognito_header = { "authorization": login_response.headers['Authorization'], 'content-type': 'application/vnd.api+json' }
 
-def cognito_chown_request(self, anonymous_ids, cognito_id):
+def cognito_chown_request(self, anonymous_ids, cognito_id, header):
   payload = { "data": { "type": "chown_requests", "attributes": { "from_ids": anonymous_ids, "to_id": cognito_id } } }
-  self.client.post('cognito/chown_requests', data=json.dumps(payload), headers=self.anonymous_header)
+  self.client.post('cognito/chown_requests', data=json.dumps(payload), headers=header)
 
-def create_and_login_as_cognito_user(self, pool_id):
+def create_and_login_as_cognito_user(self, pool_id, header):
   identifier = Faker().pystr()
-  create_cognito_user(self, identifier, pool_id)
-  login_cognito_user(self, identifier)
+  create_cognito_user(self, identifier, pool_id, header)
+  login_cognito_user(self, identifier, header)
