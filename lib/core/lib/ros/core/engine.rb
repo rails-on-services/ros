213b08ba0aeb5c854f721b211038b978cb642ef6
Peter Hoeg <peter@hoeg.com>
2019-12-30 12:46:06 +0800

    core: make the root path of the env configuration files configurable
    This way the ROS services can be told to be look for configuration files in a
    place local to the project using ROS services.

--- lib/core/lib/ros/core/engine.rb
+++ lib/core/lib/ros/core/engine.rb
@@ -35,7 +35,7 @@ module Ros
       initializer 'ros_core.load_platform_config' do |_app|
         # The location of the environment files is the parent services/.env dir
         # This dir is soft linked to the compose directory of the current deployment
-        if Ros.host_env.os? && Dir.exist?("#{Ros.root}/services/.env")
+        if Ros.host_env.os? && Dir.exist?(Ros.environment_root)
           configs = ['platform']
           ary = Settings.instance_variable_get('@config_sources').select do |config|
             config.instance_variable_get('@hash')&.keys&.include?(:service)
@@ -45,7 +45,7 @@ module Ros
           end
           require 'dotenv'
           configs.each do |env_name|
-            env_file = "#{Ros.root}/services/.env/#{env_name}.env"
+            env_file = File.join Ros.environment_root, "#{env_name}.env"
             Dotenv.load(env_file) if File.exist?(env_file)
           end
           # Set ENVs that allow the local server to access compose cluster services
