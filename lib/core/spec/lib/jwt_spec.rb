0c6752046c6624bd1ead111088d15fa82010058b
somethvictory <perx@somethvictorys-MacBook-Pro.local>
2020-01-06 18:10:44 +0800

    PM-3083 Add specs for Ros::Jwt

--- lib/core/spec/lib/jwt_spec.rb
+++ lib/core/spec/lib/jwt_spec.rb
@@ -3,5 +3,52 @@
 require 'rails_helper'
 
 RSpec.describe Ros::Jwt do
-  pending 'something'
+  let(:aud) { Settings.jwt.aud }
+  let(:iss) { Settings.jwt.iss }
+  let(:alg) { Settings.jwt.alg }
+  let(:encryption_key) { Settings.jwt.encryption_ke }
+
+  subject { described_class.new('Bearer something')}
+
+  context 'when payload is a hash' do
+    subject { described_class.new({}) }
+
+    it 'sets default payload' do
+      expect(subject.claims[:iss]).to eq "https://iam.api.development.whistler.perxtech.io"
+      expect(subject.claims[:aud]).to eq "https://api.development.whistler.perxtech.io"
+      expect(subject.claims[:iat]).to be_an_instance_of Integer
+    end
+  end
+
+  context 'when payload is not a hash' do
+    subject { described_class.new('Bearer something')}
+
+    it 'sets value to the token' do
+      expect(subject.token).to_not be_empty
+    end
+
+    it 'removes Bearer' do
+      expect(subject.token).to eq 'something'
+    end
+
+    it 'does not set claims' do
+      expect(subject.claims).to be_empty
+    end
+  end
+
+  context 'when decoding successfully' do
+    it 'assigns value to claims' do
+      # allow(JWT).to receive(:decode).with('something', encryption_key, alg).and_return([{data: {}}, {header: {}}])
+
+    end
+  end
+
+  context 'when an error occurs while decoding' do
+  end
+
+  context 'when encoding' do
+    it 'returns encoded string' do
+      expect(subject.encode).to be_an_instance_of String
+    end
+  end
 end
