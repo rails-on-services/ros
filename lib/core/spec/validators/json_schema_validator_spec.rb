b6a52a28144c2032bb795f515f6c37d4f90e0a6b
somethvictory <victory.someth@gmail.com>
2020-01-08 18:27:34 +0800

    PM-3083 Respond to some feedbacks

--- lib/core/spec/validators/json_schema_validator_spec.rb
+++ lib/core/spec/validators/json_schema_validator_spec.rb
@@ -19,62 +19,67 @@ describe JsonSchemaValidator do
   context 'when attribute respects the schema' do
     let(:properties) do
       {
-        'title' => 'TITLE',
-        'button' => 'BUTTON',
-        'closed_pinata_img_url' => 'url',
-        'no_of_taps' => 10
+        title: 'TITLE',
+        button: 'BUTTON',
+        closed_pinata_img_url: 'url',
+        no_of_taps: 10
       }
     end
 
     it 'is valid' do
-      subject.properties = properties
+      subject.properties = properties.stringify_keys
       expect(subject).to be_valid
     end
   end
 
   context 'when attribute does not respect the schema' do
+    let(:mismatched_message) { 'does not match specified schema: missing_keys: title, button, closed_pinata_img_url, no_of_taps' }
     it 'is invalid when title is missing' do
       subject.properties = {
-        'button' => 'BUTTON',
-        'closed_pinata_img_url' => 'url',
-        'no_of_taps' => 10
+        button: 'BUTTON',
+        closed_pinata_img_url: 'url',
+        no_of_taps: 10
       }
 
+      subject.properties = properties.stringify_keys
       expect(subject).to be_invalid
-      expect(subject.errors.messages[:properties].first).to eq 'JSON schema mismatched'
+      expect(subject.errors.messages[:properties].first).to eq mismatched_message
     end
 
     it 'is invalid when button is missing' do
       subject.properties = {
-        'title' => 'TITLE',
-        'closed_pinata_img_url' => 'url',
-        'no_of_taps' => 10
+        title: 'TITLE',
+        closed_pinata_img_url: 'url',
+        no_of_taps: 10
       }
 
+      subject.properties = properties.stringify_keys
       expect(subject).to be_invalid
-      expect(subject.errors.messages[:properties].first).to eq 'JSON schema mismatched'
+      expect(subject.errors.messages[:properties].first).to eq mismatched_message
     end
 
     it 'is invalid when button is closed_pinata_img_url is missing' do
       subject.properties = {
-        'title' => 'TITLE',
-        'button' => 'BUTTON',
-        'no_of_taps' => 10
+        title: 'TITLE',
+        button: 'BUTTON',
+        no_of_taps: 10
       }
 
+      subject.properties = properties.stringify_keys
       expect(subject).to be_invalid
-      expect(subject.errors.messages[:properties].first).to eq 'JSON schema mismatched'
+      expect(subject.errors.messages[:properties].first).to eq mismatched_message
     end
 
     it 'is invalid when button is no_of_taps is missing' do
       subject.properties = {
-        'title' => 'TITLE',
-        'button' => 'BUTTON',
-        'closed_pinata_img_url' => 'url'
+        title: 'TITLE',
+        button: 'BUTTON',
+        closed_pinata_img_url: 'url'
       }
 
+      subject.properties = properties.stringify_keys
       expect(subject).to be_invalid
-      expect(subject.errors.messages[:properties].first).to eq 'JSON schema mismatched'
+      expect(subject.errors.messages[:properties].first).to eq mismatched_message
     end
   end
 end
