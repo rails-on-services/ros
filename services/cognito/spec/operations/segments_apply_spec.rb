8d513222fb782c6b0d32e6f8e96695c54e833612
Rui Baltazar <rui.p.baltazar@gmail.com>
2020-01-09 14:05:29 +0800

    WIP - working on applying segmetns wiht failure

--- services/cognito/spec/operations/segments_apply_spec.rb
+++ services/cognito/spec/operations/segments_apply_spec.rb
@@ -4,14 +4,14 @@ require 'rails_helper'
 
 RSpec.describe SegmentsApply, type: :operation do
   let(:op_result) { described_class.call(op_params) }
+  let(:op_params) { { users: User.all, segments: segments } }
+  let(:segments) { {} }
 
   before do
     create_list(:user, 5)
   end
 
   context 'no segments are passed' do
-    let(:op_params) { { users: User.all, segments: {} } }
-
     it 'returns successful result' do
       expect(op_result.success?).to eq true
       expect(op_result.model.size).to eq(5)
@@ -19,15 +19,28 @@ RSpec.describe SegmentsApply, type: :operation do
   end
 
   context 'multiple segments are sent as attribute' do
-    let(:op_params) { { users: User.all, segments: { 'bananas': '10', 'gender': 'male' } } }
+    context 'when one segment does not exist' do
+      let(:segments) { { 'bananas': '10', 'gender': 'male' }.as_json }
 
-    it 'returns failure result' do
-      expect(op_result.failure?).to eq true
-      expect(op_result.model).to eq nil
-      expect(op_result.errors.full_messages).to eq ["Segment can't find segmentation class"]
+      it 'returns failure result' do
+        expect(op_result.failure?).to eq true
+        expect(op_result.model).to eq nil
+        expect(op_result.errors.full_messages).to eq ["Segment can't find segmentation class"]
+      end
+    end
+
+    context 'when one segment fails to apply' do
+      let(:segments) { { 'age': { 'from': '10', 'to': '15' }, 'gender': 'male' }.as_json }
+
+      before do
+        allow(Segments::Age).to receive(:call).and_return(OpenStruct.new("failure?": true))
+      end
+
+      it 'returns failure result' do
+        expect(op_result.failure?).to eq true
+        expect(op_result.model).to eq nil
+        expect(op_result.errors.full_messages).to eq ["Segment can't find segmentation class"]
+      end
     end
   end
-
-  # TODO: cover case where multiple segments are passed and one of them fails
-  # TODO: cover case where segment is inexistent (NameError raised)
 end
