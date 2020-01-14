866b0d82d34c39ad0426c7a03d61c4d4b8c67c8a
somethvictory <victory.someth@gmail.com>
2020-01-14 14:29:37 +0800

    PM-3143: Remove recipient_id from message

--- services/comm/db/migrate/20200110055523_add_recipient_id_to_messages.rb
+++
@@ -1,5 +0,0 @@
-class AddRecipientIdToMessages < ActiveRecord::Migration[6.0]
-  def change
-    add_column :messages, :recipient_id, :integer
-  end
-end
--- services/comm/spec/dummy/db/schema.rb
+++ services/comm/spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_10_055523) do
+ActiveRecord::Schema.define(version: 2019_11_27_070152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,6 @@ ActiveRecord::Schema.define(version: 2020_01_10_055523) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "provider_msg_id"
-    t.integer "recipient_id"
     t.index ["owner_type", "owner_id"], name: "index_messages_on_owner_type_and_owner_id"
     t.index ["provider_id"], name: "index_messages_on_provider_id"
   end
