cbc2fa0d57bc26eb20a5aa9964a6ea9d427f32d3
Rui Baltazar <rui.p.baltazar@gmail.com>
2020-01-16 18:20:33 +0800

    [Hotfix] working on chown

--- services/cognito/app/models/chown_request.rb
+++ services/cognito/app/models/chown_request.rb
@@ -3,11 +3,12 @@
 class ChownRequest < Cognito::ApplicationRecord
   has_many :chown_results
 
-  after_commit :create_chown_results, on: :create
+  after_commit :publish_create_event, on: :create
 
   private
 
-  def create_chown_results
-    ChownRequestProcess.call(id: id, from_ids: from_ids, to_id: to_id)
+  def publish_create_event
+    publish_event('chown_created')
+    # ChownRequestProcess.call(id: id, from_ids: from_ids, to_id: to_id)
   end
 end
