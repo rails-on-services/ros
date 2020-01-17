21a826c44b71680feb662457b8e4f8881a900743
Sergey Toy <sergeytoy@perxtech.com>
2020-01-17 17:47:48 +0800

    add sad path to operation and specs

--- services/comm/app/operations/message_process.rb
+++ services/comm/app/operations/message_process.rb
@@ -2,10 +2,16 @@
 
 class MessageProcess < Ros::ActivityBase
   step :create_message
+  failed :cannot_create_message
 
   private
 
-  def create_message(_ctx, params:, **)
-    MessageCreate.call(params: params)
+  def create_message(ctx, params:, **)
+    ctx[:op_result] = MessageCreate.call(params: params)
+    ctx[:op_result].success?
+  end
+
+  def cannot_create_message(_ctx, op_result:, errors:, **)
+    errors.add(:message, op_result.errors.full_messages.join(', '))
   end
 end
