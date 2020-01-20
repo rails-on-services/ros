48db9c99c4e04b267656ac72136881aba599aa67
Rui Baltazar <rui.p.baltazar@gmail.com>
2020-01-20 13:30:05 +0800

    WIP - made karafka publisher available in core

--- lib/core/lib/ros/karafka_publisher.rb
+++ lib/core/lib/ros/karafka_publisher.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
 module Ros
-  module KarafkaPublisher
-    def defaults
-      { schema_name: Apartment::Tenant.current }
-    end
+  class KarafkaPublisher
+    class << self
+      def defaults
+        { schema_name: Apartment::Tenant.current }
+      end
 
-    def kafka_message(data = {})
-      defaults.merge(data).to_json
-    end
+      def kafka_message(data = {})
+        defaults.merge(data).to_json
+      end
 
-    def publish_to(topic, data = {})
-      message = kafka_message(data)
-      Rails.logger.log "[Karafka::Publisher][#{topic}]: #{message}"
-      WaterDrop::SyncProducer.call(message, topic: topic)
-      true
+      def publish_to(topic, data = {})
+        message = kafka_message(data)
+        Rails.logger.debug "[Karafka::Publisher][#{topic}]: #{message}"
+        WaterDrop::SyncProducer.call(message, topic: topic)
+        true
+      end
     end
   end
 end
