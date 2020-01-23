eb05735b91d8682a6fb8f0f21d33b3b539f54661
Kostya <kostya@perxtech.com>
2020-01-23 17:19:45 +0800

    Karafka. Fix seed_brokers array init

--- services/cognito/karafka.rb
+++ services/cognito/karafka.rb
@@ -23,7 +23,13 @@ class KarafkaApp < Karafka::App
     kafka_enabled = Settings.dig(:infra, :services, :kafka, :enabled)
     next unless kafka_enabled
 
-    config.kafka.seed_brokers = [Settings.infra.services.kafka.seed_brokers]
+    config.kafka.seed_brokers = Settings.infra.services.kafka.bootstrap_servers.split(',').map do |broker|
+      broker = "kafka://#{broker}" unless broker.starts_with? "kafka://"
+    end
+    if Settings.infra.services.kafka.security_protocol == 'SASL_SSL' && Settings.infra.services.kafka.sasl_mechanism == 'PLAIN'
+      config.kafka.sasl_plain_username = Settings.infra.services.kafka.username
+      config.kafka.sasl_plain_password = Settings.infra.services.kafka.password
+    end
     config.client_id = 'cognito-service'
     config.logger = Rails.logger
   end
