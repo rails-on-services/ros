f2cb07aab6a8e213aace63a8bca5fa0308b587d2
somethvictory <victory.someth@gmail.com>
2020-01-23 17:00:03 +0800

    PW-3338: Adding specs

--- services/cognito/spec/models/tenant_spec.rb
+++ services/cognito/spec/models/tenant_spec.rb
@@ -5,8 +5,14 @@ require 'rails_helper'
 RSpec.describe Tenant, type: :model do
   include_examples 'application record concern' do
     let(:tenant) { Tenant.first }
-    let(:subject) { tenant }
+    subject do
+      # update schema to test uniqueness
+      tenant.schema_name = '111_111_aaa'
+      tenant.save(validate: false)
+      tenant
+    end
   end
 
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:schema_name) }
+  it { is_expected.to validate_uniqueness_of(:schema_name) }
 end
