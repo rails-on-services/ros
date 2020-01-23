10245447f52674e609119126a1d528b7d02137fc
Sergey Toy <sergeytoy@perxtech.com>
2020-01-23 12:41:39 +0800

    update factories

--- services/comm/spec/factories/providers.rb
+++ services/comm/spec/factories/providers.rb
@@ -5,11 +5,13 @@ FactoryBot.define do
     name { 'AWS' }
     access_key_id { 'access_key_id' }
     secret_access_key { 'secret_access_key' }
+    channels { ['sms'] }
   end
 
   factory :provider_twilio, class: 'Providers::Twilio' do
     name { 'Twilio' }
     account_sid { 'sid' }
     auth_token { 'token' }
+    channels { %w[sms call] }
   end
 end
