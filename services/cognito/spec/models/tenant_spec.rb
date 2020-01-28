7b15e86c84e63faf56cee732161be4b8a15d6c40
somethvictory <victory.someth@gmail.com>
2020-01-28 18:10:28 +0800

    PW-3338: Ignore case sensitivy in spec

--- services/cognito/spec/models/tenant_spec.rb
+++ services/cognito/spec/models/tenant_spec.rb
@@ -5,14 +5,9 @@ require 'rails_helper'
 RSpec.describe Tenant, type: :model do
   include_examples 'application record concern' do
     let(:tenant) { Tenant.first }
-    subject do
-      # update schema to test uniqueness
-      tenant.schema_name = '111_111_aaa'
-      tenant.save(validate: false)
-      tenant
-    end
+    subject { tenant }
   end
 
   it { is_expected.to validate_presence_of(:schema_name) }
-  it { is_expected.to validate_uniqueness_of(:schema_name) }
+  it { is_expected.to validate_uniqueness_of(:schema_name).ignoring_case_sensitivity }
 end
